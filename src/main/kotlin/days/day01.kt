872ea785b9e1870961bf559ade2878be5aa8c919
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2018-12-02 12:43:47 +0100

    Minor cleanup

--- src/main/kotlin/days/day01.kt
+++ src/main/kotlin/days/day01.kt
@@ -1,18 +1,17 @@
 package days
 
 fun day01a(changes: List<String>): Int {
-    return changes.fold(0) { frequency, change ->
-        frequency + change.toInt()
-    }
+    return changes.sumBy { it.toInt() }
 }
 
 fun day01b(changes: List<String>): Int {
     val visited: MutableSet<Int> = mutableSetOf()
-    val changesInfiniteSequence = generateSequence(0) { (it + 1) % changes.count() }
-    return changesInfiniteSequence.fold(0) { frequency, changeIndex ->
+    val changesInfiniteSequence = generateSequence(0) { (it + 1) % changes.count() }.map { changes[it] }
+    return changesInfiniteSequence.fold(0)
+        { frequency, change ->
             if (visited.contains(frequency))
                 return frequency
             visited.add(frequency)
-            frequency + changes[changeIndex].toInt()
+            frequency + change.toInt()
         }
 }
