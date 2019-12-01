9bc4686d320830b8e7fc0487ed1795ffe894732f
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-01 10:50:09 +0100

    Used cleaner sequence solution for day 1 B

--- src/main/kotlin/v2019/days/day01.kt
+++ src/main/kotlin/v2019/days/day01.kt
@@ -3,24 +3,22 @@ package v2019.days.day01
 import v2019.sumByLong
 import kotlin.math.max
 
-private val computeRequiredFuel = { mass: Long -> max(mass / 3 - 2, 0) }
-
-private fun recurseUntilZero(initialValue: Long, computeFunc: (Long) -> Long): Long {
-    if (initialValue <= 0L) {
-        return 0
-    }
-    val value = computeFunc(initialValue)
-    return value + recurseUntilZero(value, computeFunc)
+private fun computeRequiredFuel(mass: Long): Long {
+    return max(mass / 3 - 2, 0)
 }
 
 fun day01a(input: List<String>): Long {
     return input
         .map { it.toLong() }
-        .sumByLong { mass -> computeRequiredFuel(mass) }
+        .sumByLong(::computeRequiredFuel)
 }
 
 fun day01b(input: List<String>): Long {
     return input
         .map { it.toLong() }
-        .sumByLong { mass -> recurseUntilZero(mass, computeRequiredFuel) }
+        .sumByLong { mass ->
+            generateSequence(computeRequiredFuel(mass), ::computeRequiredFuel)
+                .takeWhile { it > 0L }
+                .sum()
+        }
 }
