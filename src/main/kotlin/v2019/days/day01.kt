efb2e75e7b63bcd88931bd9d404c18648d2d4918
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-01 10:46:22 +0100

    Refactored day 01 B to use recursion instead of sequences

--- src/main/kotlin/v2019/days/day01.kt
+++ src/main/kotlin/v2019/days/day01.kt
@@ -3,8 +3,14 @@ package v2019.days.day01
 import v2019.sumByLong
 import kotlin.math.max
 
-private fun computeRequiredFuel(mass: Long): Long {
-    return max(mass / 3 - 2, 0)
+private val computeRequiredFuel = { mass: Long -> max(mass / 3 - 2, 0) }
+
+private fun recurseUntilZero(initialValue: Long, computeFunc: (Long) -> Long): Long {
+    if (initialValue <= 0L) {
+        return 0
+    }
+    val value = computeFunc(initialValue)
+    return value + recurseUntilZero(value, computeFunc)
 }
 
 fun day01a(input: List<String>): Long {
@@ -16,10 +22,5 @@ fun day01a(input: List<String>): Long {
 fun day01b(input: List<String>): Long {
     return input
         .map { it.toLong() }
-        .sumByLong { mass ->
-            generateSequence(
-                computeRequiredFuel(mass),
-                { fuelMass -> if (fuelMass == 0L) null else computeRequiredFuel(fuelMass) }
-            ).sum()
-        }
+        .sumByLong { mass -> recurseUntilZero(mass, computeRequiredFuel) }
 }
