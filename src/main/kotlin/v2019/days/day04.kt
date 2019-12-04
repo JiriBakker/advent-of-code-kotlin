8c12b9373126e93fa04cb8139aacfdb66205a890
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-04 14:15:13 +0100

    Refactored Day 4 to generate ascending numbers instead of brute-forcing

--- src/main/kotlin/v2019/days/day04.kt
+++ src/main/kotlin/v2019/days/day04.kt
@@ -15,15 +15,44 @@ private fun isValidNr(number: Long, condition: (Int) -> Boolean): Boolean {
     return hasDouble && isIncreasing
 }
 
+private fun List<Int>.joinToLong(): Long {
+    return this.joinToString("").toLong()
+}
+
+private fun generateAscendingNumbers(digits: List<Int>, maxLength: Int): List<Long> {
+    if (digits.size == maxLength) {
+        return listOf(digits.joinToLong())
+    }
+
+    return IntRange(digits.last(), 9)
+        .flatMap { digit -> generateAscendingNumbers(digits.toMutableList().plus(digit), maxLength) }
+}
+
+private fun firstDigit(number: Long): Int {
+    return Character.getNumericValue(number.toString().first())
+}
+
+private fun findAscendingNumbersInRange(min: Long, max: Long): List<Long> {
+    val maxLength = max.toString().length
+    val lowestDigit = if (maxLength > min.toString().length) 0 else firstDigit(min)
+    return IntRange(lowestDigit, firstDigit(max))
+        .flatMap { digit -> generateAscendingNumbers(listOf(digit), maxLength) }
+        .filter { nr -> nr in min..max }
+}
+
 fun day04a(input: String): Int {
     val (min, max) = input.split("-").map(String::toLong)
 
-    return LongRange(min, max).filter { nr -> isValidNr(nr) { it == it } }.size
+    val ascendingNumbers = findAscendingNumbersInRange(min, max)
+
+    return ascendingNumbers.filter { nr -> isValidNr(nr) { it == it } }.size
 }
 
 fun day04b(input: String): Int {
-    val (min, max) = input.split("-").map(String::toLong)
+     val (min, max) = input.split("-").map(String::toLong)
 
-    return LongRange(min, max).filter { nr -> isValidNr(nr) { it == 1 } }.size
+    val ascendingNumbers = findAscendingNumbersInRange(min, max)
+
+    return ascendingNumbers.filter { nr -> isValidNr(nr) { it == 1 } }.size
 }
 
