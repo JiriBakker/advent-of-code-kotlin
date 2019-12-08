acf1169904d7ab85492a1a3c2af72bbf75b0edd0
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-08 10:21:42 +0100

    Refactored Day 8

--- src/main/kotlin/v2019/days/day08.kt
+++ src/main/kotlin/v2019/days/day08.kt
@@ -1,12 +1,16 @@
 package v2019.days.day08
 
-private fun parseLayers(input: String, width: Int, height: Int): List<List<Int>> {
-    return input.map(Character::getNumericValue).chunked(width * height)
+private fun parseLayers(input: String, width: Int, height: Int): List<String> {
+    return input.chunked(width * height)
 }
 
-private fun List<Int>.pixelsToString(width: Int): String {
+private const val BLACK = '0'
+private const val WHITE = '1'
+private const val TRANSPARENT = '2'
+
+private fun List<Char>.pixelsToString(width: Int): String {
     return this
-        .joinToString("") { if (it == 1) "█" else " " }
+        .joinToString("") { if (it == WHITE) "█" else " " }
         .chunked(width)
         .joinToString("\n")
 }
@@ -14,18 +18,17 @@ private fun List<Int>.pixelsToString(width: Int): String {
 fun day08a(input: String, width: Int = 25, height: Int = 6): Int {
     val layers = parseLayers(input, width, height)
 
-    val layerWithLeastZeroes =
-        layers
-            .map { layer -> layer.groupingBy { digit -> digit }.eachCount() }
-            .minBy { counts -> counts[0] ?: Int.MAX_VALUE }!!
+    return layers
+        .map { layer -> layer.groupingBy { digit -> digit }.eachCount() }
+        .minBy { counts -> counts[BLACK] ?: Int.MAX_VALUE }
+        .let { layer -> (layer!![WHITE] ?: 0) * (layer[TRANSPARENT] ?: 0) }
 
-    return (layerWithLeastZeroes[1] ?: 0) * (layerWithLeastZeroes[2] ?: 0)
 }
 
 fun day08b(input: String, width: Int = 25, height: Int = 6): String {
     val layers = parseLayers(input, width, height)
 
     return IntRange(0, width * height - 1)
-        .map { index -> layers.first { it[index] != 2 }[index] }
+        .map { index -> layers.first { it[index] != TRANSPARENT }[index] }
         .pixelsToString(width)
 }
