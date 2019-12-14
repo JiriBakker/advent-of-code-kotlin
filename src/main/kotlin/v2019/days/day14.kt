b376b20ce0f37f666f4cab1e57390153ad23eb1c
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-14 12:33:08 +0100

    Day 14: cleaned up naming and leftovers interaction

--- src/main/kotlin/v2019/days/day14.kt
+++ src/main/kotlin/v2019/days/day14.kt
@@ -4,70 +4,59 @@ import v2019.util.BinarySearchRange
 import v2019.util.doBinarySearch
 import java.util.ArrayDeque
 import kotlin.math.ceil
+import kotlin.math.max
 import kotlin.math.min
 
 private const val ORE = "ORE"
 private const val FUEL = "FUEL"
 
-private class Material(val name: String, val amount: Long)
-private class Reaction(val produces: Material, val requirements: List<Material>)
+private class Chemical(val name: String, val amount: Long)
+private class Reaction(val produces: Chemical, val requirements: List<Chemical>)
 
 private fun parseReactions(input: List<String>): Map<String, Reaction> {
-    fun parseMaterial(str: String): Material {
-        return str.split(" ").let { Material(it[1], it[0].toLong()) }
+    fun parseChemical(str: String): Chemical {
+        return str.split(" ").let { Chemical(it[1], it[0].toLong()) }
     }
 
     return input
         .associate {
             val segments = it.split(" => ")
-            val requirements = segments[0].split(", ").map(::parseMaterial)
-            val produces = parseMaterial(segments[1])
+            val requirements = segments[0].split(", ").map(::parseChemical)
+            val produces = parseChemical(segments[1])
             produces.name to Reaction(produces, requirements)
         }
 }
 
 private fun findOreRequired(reactions: Map<String, Reaction>, fuelRequired: Long = 1L): Long {
-    val toProduce = ArrayDeque<Pair<Material, Long>>(
+    val toProduce = ArrayDeque<Pair<Chemical, Long>>(
         listOf(reactions[FUEL]!!.produces to fuelRequired)
     )
 
     var oreRequired = 0L
     val leftovers = mutableMapOf<String, Long>()
 
-    fun useLeftoversIfAvailable(materialName: String, amountRequired: Long): Long {
-        val leftoversAvailable = leftovers.getOrDefault(materialName, 0L)
-        val leftoversUsed = min(leftoversAvailable, amountRequired)
-
-        leftovers[materialName] = leftoversAvailable - leftoversUsed
-
-        return leftoversUsed
-    }
-
-    fun storeLeftovers(materialName: String, amountProduced: Long, amountUsed: Long) {
-        leftovers[materialName] =
-            leftovers.getOrDefault(materialName, 0L) +
-                (amountProduced - amountUsed)
-    }
-
     while (toProduce.isNotEmpty()) {
-        val (material, amountRequired) = toProduce.poll()
+        val (chemical, grossRequired) = toProduce.poll()
 
-        if (material.name == ORE) {
-            oreRequired += amountRequired
+        if (chemical.name == ORE) {
+            oreRequired += grossRequired
             continue
         }
 
-        val reaction = reactions[material.name] ?: error("No reaction found for ${material.name}")
+        val reaction = reactions[chemical.name] ?: error("No reaction found for ${chemical.name}")
 
-        val leftoversUsed = useLeftoversIfAvailable(material.name, amountRequired)
-        val amountRequiredAfterLeftovers = amountRequired - leftoversUsed
-        val reactionCount = ceil(amountRequiredAfterLeftovers.toDouble() / reaction.produces.amount).toLong()
+        val leftover = leftovers[chemical.name] ?: 0
+        val leftoverUsed = min(leftover, grossRequired)
+        leftovers[chemical.name] = max(0, leftover - leftoverUsed)
 
-        if (amountRequiredAfterLeftovers <= 0L) {
+        val netRequired = grossRequired - leftoverUsed
+        if (netRequired <= 0L) {
             continue
         }
 
-        storeLeftovers(material.name, reaction.produces.amount * reactionCount, amountRequiredAfterLeftovers)
+        val reactionCount = ceil(netRequired.toDouble() / reaction.produces.amount).toLong()
+
+        leftovers[chemical.name] = reaction.produces.amount * reactionCount - netRequired
 
         toProduce.addAll(reaction.requirements.map { it to it.amount * reactionCount })
     }
