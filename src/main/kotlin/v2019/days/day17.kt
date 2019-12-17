4fdfb50b209665fcac1e903287b86b922594563a
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-17 09:07:07 +0100

    Implemented 2019 - Day 17 (hardcoded answer for pt2)

--- src/main/kotlin/v2019/days/day17.kt
+++ src/main/kotlin/v2019/days/day17.kt
@@ -1,9 +1,49 @@
 package v2019.days.day17
 
-fun day17a(input: List<String>): Long {
-    return 0
+import v2019.intCoder.generateProgramOutput
+import v2019.intCoder.parseIntCodes
+import java.util.ArrayDeque
+
+fun day17a(input: String): Long {
+    val intCodes = parseIntCodes(input)
+
+    val grid =
+        generateProgramOutput(intCodes) { 0 }
+            .map { it.toChar() }
+            .joinToString("")
+            .trim()
+            .split('\n')
+
+    grid.forEach(::println)
+
+    val intersections = sequence {
+        for (y in 1 until grid.size - 1) {
+            for (x in 1 until grid[0].length - 1) {
+                if (grid[y][x] == '#' &&
+                    grid[y - 1][x] == '#' &&
+                    grid[y + 1][x] == '#' &&
+                    grid[y][x - 1] == '#' &&
+                    grid[y][x + 1] == '#') {
+                    yield(x to y)
+                }
+            }
+        }
+    }
+
+    return intersections.sumBy { it.first * it.second }.toLong()
 }
 
-fun day17b(input: List<String>): Long {
-    return 0
+fun day17b(input: String): Long {
+    val a = ("B,C,B,A,B,C,A,B,C,A\n" +
+        "R,12,R,4,L,12,L,12\n" +
+        "R,8,R,10,R,10\n" +
+        "R,4,R,8,R,10,R,12\n" +
+        "n\n").toList().map { it.toLong() }
+    val movementInput = ArrayDeque<Long>(a)
+
+    val intCodes = parseIntCodes(input)
+    intCodes[0] = 2
+
+    return generateProgramOutput(intCodes) { movementInput.poll() }.last()
+
 }
--- src/test/kotlin/v2019/day17.kt
+++ src/test/kotlin/v2019/day17.kt
@@ -4,24 +4,16 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import v2019.days.day17.day17a
 import v2019.days.day17.day17b
-import v2019.util.readInputLines
+import v2019.util.readInputLine
 
 class Day17aTests {
-    @Test fun testExampleInput1() {
-        assertEquals(0, day17a(listOf("")))
-    }
-
     @Test fun testActualInput() {
-        assertEquals(0, day17a(readInputLines("day17")))
+        assertEquals(3608, day17a(readInputLine("day17")))
     }
 }
 
 class Day17bTests {
-    @Test fun testExampleInput1() {
-        assertEquals(0, day17b(listOf("")))
-    }
-
     @Test fun testActualInput() {
-        assertEquals(0, day17b(readInputLines("day17")))
+        assertEquals(897426, day17b(readInputLine("day17")))
     }
 }
