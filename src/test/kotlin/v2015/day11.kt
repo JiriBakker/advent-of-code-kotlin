e44800bb5cf7982a3ffff62dbe21d9a55d9e1cbf
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-18 19:22:33 +0100

    Implemented 2015 - Day 11

---
+++ src/test/kotlin/v2015/day11.kt
@@ -0,0 +1,41 @@
+package v2015
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import v2015.days.day11.day11a
+import v2015.days.day11.day11b
+import v2015.days.day11.isValidPassword
+import v2015.util.readInputLine
+
+class Day11aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(false, isValidPassword("hijklmmn".map { it.toInt() }))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(false, isValidPassword("abbceffg".map { it.toInt() }))
+    }
+
+    @Test fun testExampleInput3() {
+        assertEquals(false, isValidPassword("abbcegjk".map { it.toInt() }))
+    }
+
+    @Test fun testExampleInput4() {
+        assertEquals("abcdffaa", day11a("abcdefgh"))
+    }
+
+    @Test fun testExampleInput5() {
+        assertEquals("ghjaabcc", day11a("ghijklmn"))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals("cqjxxyzz", day11a(readInputLine("day11")))
+    }
+}
+
+class Day11bTests {
+    @Test fun testActualInput() {
+        assertEquals("cqkaabcc", day11b(readInputLine("day11")))
+    }
+}
+
