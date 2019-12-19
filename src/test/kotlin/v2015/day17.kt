f961fde401742a883a631a391c458a664ea0731e
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-19 19:25:53 +0100

    Implemented 2015 - Day 17

---
+++ src/test/kotlin/v2015/day17.kt
@@ -0,0 +1,27 @@
+package v2015
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import v2015.days.day17.day17a
+import v2015.days.day17.day17b
+import v2015.util.readInputLines
+
+class Day17aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(4, day17a(listOf("20", "15", "10", "5", "5"), 25))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(1638, day17a(readInputLines("day17")))
+    }
+}
+
+class Day17bTests {
+    @Test fun testExampleInput1() {
+        assertEquals(3, day17b(listOf("20", "15", "10", "5", "5"), 25))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(17, day17b(readInputLines("day17")))
+    }
+}
