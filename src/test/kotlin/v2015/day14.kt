8e4b2ef4a100ba7b54d8cea397cec06b9f48afdb
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-19 17:55:02 +0100

    Implemented 2015 - Day 14

---
+++ src/test/kotlin/v2015/day14.kt
@@ -0,0 +1,31 @@
+package v2015
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import v2015.days.day14.day14a
+import v2015.days.day14.day14b
+import v2015.util.readInputLines
+
+class Day14aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(1120, day14a(listOf(
+            "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+            "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."), 1000))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(2660, day14a(readInputLines("day14")))
+    }
+}
+
+class Day14bTests {
+    @Test fun testExampleInput1() {
+        assertEquals(689, day14b(listOf(
+            "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+            "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."), 1000))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(1256, day14b(readInputLines("day14")))
+    }
+}
