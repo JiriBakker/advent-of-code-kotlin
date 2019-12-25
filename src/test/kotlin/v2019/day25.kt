528bfee6f60748e67f3ada09859b9c5290ddb503
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-25 12:12:10 +0100

    Ignore 2019 day 25 test

--- src/test/kotlin/v2019/day25.kt
+++ src/test/kotlin/v2019/day25.kt
@@ -1,16 +1,14 @@
 package v2019
 
 import org.junit.Assert.assertEquals
+import org.junit.Ignore
 import org.junit.Test
 import v2019.days.day25.day25a
-import v2019.util.readInputLines
+import v2019.util.readInputLine
 
 class Day25aTests {
-    @Test fun testExampleInput1() {
-        assertEquals(0, day25a(listOf("")))
-    }
-
+    @Ignore // Requires manual input
     @Test fun testActualInput() {
-        assertEquals(0, day25a(readInputLines("day25")))
+        assertEquals(0, day25a(readInputLine("day25")))
     }
 }
