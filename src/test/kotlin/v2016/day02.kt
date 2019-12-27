8729942e9f8b75c3626c4139077625ad9b961b25
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-27 13:04:56 +0100

    Implemented 2016 Day 2

---
+++ src/test/kotlin/v2016/day02.kt
@@ -0,0 +1,35 @@
+package v2016.days.day02
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import util.readInputLines
+
+class Day02aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(1985, day02a(listOf(
+            "ULL",
+            "RRDDD",
+            "LURDL",
+            "UUUUD"
+        )))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(99332, day02a(readInputLines("day02", 2016)))
+    }
+}
+
+class Day02bTests {
+    @Test fun testExampleInput1() {
+        assertEquals("5DB3", day02b(listOf(
+            "ULL",
+            "RRDDD",
+            "LURDL",
+            "UUUUD"
+        )))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals("DD483", day02b(readInputLines("day02", 2016)))
+    }
+}
