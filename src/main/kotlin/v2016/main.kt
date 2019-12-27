5e0bab38b579ea84e6b0d81200fbcfbc08725b36
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-27 17:31:16 +0100

    Added 2016 Day 4 to main

--- src/main/kotlin/v2016/main.kt
+++ src/main/kotlin/v2016/main.kt
@@ -1,8 +1,15 @@
 package v2016
 
 import util.readInputLine
+import util.readInputLines
 import v2016.days.day01.day01a
 import v2016.days.day01.day01b
+import v2016.days.day02.day02a
+import v2016.days.day02.day02b
+import v2016.days.day03.day03a
+import v2016.days.day03.day03b
+import v2016.days.day04.day04a
+import v2016.days.day04.day04b
 import kotlin.system.measureTimeMillis
 
 private fun time(func: () -> Unit) {
@@ -13,4 +20,10 @@ private fun time(func: () -> Unit) {
 fun main() {
     time { print("Day01a: " + day01a(readInputLine("day01", 2016))) }
     time { print("Day01b: " + day01b(readInputLine("day01", 2016))) }
+    time { print("Day02a: " + day02a(readInputLines("day02", 2016))) }
+    time { print("Day02b: " + day02b(readInputLines("day02", 2016))) }
+    time { print("Day03a: " + day03a(readInputLines("day03", 2016))) }
+    time { print("Day03b: " + day03b(readInputLines("day03", 2016))) }
+    time { print("Day04a: " + day04a(readInputLines("day04", 2016))) }
+    time { print("Day04b: " + day04b(readInputLines("day04", 2016))) }
 }
