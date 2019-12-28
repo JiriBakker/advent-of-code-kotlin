30b66a7b32b5c66f99ef42c9eaabd6f3e74cb66f
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2019-12-28 11:26:58 +0100

    Implemented 2016 Day 8

---
+++ src/test/kotlin/v2016/day08.kt
@@ -0,0 +1,33 @@
+package v2016.days.day08
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import util.readInputLines
+
+class Day08aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(6, day08a(listOf(
+            "rect 3x2",
+            "rotate column x=1 by 1",
+            "rotate row y=0 by 4",
+            "rotate column x=1 by 1"
+        )))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(119, day08a(readInputLines("day08", 2016)))
+    }
+}
+
+class Day08bTests {
+    @Test fun testActualInput() {
+        assertEquals("" +
+            "████ ████ █  █ ████  ███ ████  ██   ██  ███   ██            \n" +
+            "   █ █    █  █ █    █    █    █  █ █  █ █  █ █  █           \n" +
+            "  █  ███  ████ ███  █    ███  █  █ █    █  █ █  █           \n" +
+            " █   █    █  █ █     ██  █    █  █ █ ██ ███  █  █           \n" +
+            "█    █    █  █ █       █ █    █  █ █  █ █    █  █           \n" +
+            "████ █    █  █ █    ███  █     ██   ███ █     ██            \n",
+            day08b(readInputLines("day08", 2016)))
+    }
+}
