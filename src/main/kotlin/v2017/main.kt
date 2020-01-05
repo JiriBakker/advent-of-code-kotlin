d3bcbc0f97285b1b125d1e4ab1e9788f4a4533f2
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-01-05 09:31:43 +0100

    Implemented 2017 Day 2

--- src/main/kotlin/v2017/main.kt
+++ src/main/kotlin/v2017/main.kt
@@ -2,6 +2,8 @@ package v2017
 
 import util.readInputLine
 import util.readInputLines
+import v2016.days.day02.day02a
+import v2016.days.day02.day02b
 import v2017.days.day01.day01a
 import v2017.days.day01.day01b
 import kotlin.system.measureTimeMillis
@@ -14,4 +16,6 @@ private fun time(func: () -> Unit) {
 fun main() {
     time { print("Day01a: " + day01a(readInputLine("day01", 2017))) }
     time { print("Day01b: " + day01b(readInputLine("day01", 2017))) }
+    time { print("Day02a: " + day02a(readInputLines("day02", 2017))) }
+    time { print("Day02b: " + day02b(readInputLines("day02", 2017))) }
 }
---
+++ src/test/kotlin/v2017/day02.kt
@@ -0,0 +1,33 @@
+package v2017.days.day02
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import util.readInputLines
+
+class Day02aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(18, day02a(listOf(
+            "5 1 9 5",
+            "7 5 3",
+            "2 4 6 8"
+        )))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(54426, day02a(readInputLines("day02", 2017)))
+    }
+}
+
+class Day02bTests {
+    @Test fun testExampleInput1() {
+        assertEquals(9, day02b(listOf(
+            "5 9 2 8",
+            "9 4 7 3",
+            "3 8 6 5"
+        )))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(333, day02b(readInputLines("day02", 2017)))
+    }
+}
