86ed99bce7db9bb0819a74a18428fd291817703a
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-01-05 17:09:10 +0100

    Implemented 2017 Day 4

--- src/main/kotlin/v2017/main.kt
+++ src/main/kotlin/v2017/main.kt
@@ -8,6 +8,8 @@ import v2017.days.day02.day02a
 import v2017.days.day02.day02b
 import v2017.days.day03.day03a
 import v2017.days.day03.day03b
+import v2017.days.day04.day04a
+import v2017.days.day04.day04b
 import kotlin.system.measureTimeMillis
 
 private fun time(func: () -> Unit) {
@@ -22,4 +24,6 @@ fun main() {
     time { print("Day02b: " + day02b(readInputLines("day02", 2017))) }
     time { print("Day03a: " + day03a(readInputLine("day03", 2017))) }
     time { print("Day03b: " + day03b(readInputLine("day03", 2017))) }
+    time { print("Day04a: " + day04a(readInputLines("day04", 2017))) }
+    time { print("Day04b: " + day04b(readInputLines("day04", 2017))) }
 }
---
+++ src/test/kotlin/v2017/day04.kt
@@ -0,0 +1,49 @@
+package v2017.days.day04
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import util.readInputLines
+
+class Day04aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(1, day04a(listOf("aa bb cc dd ee")))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(0, day04a(listOf("aa bb cc dd aa")))
+    }
+
+    @Test fun testExampleInput3() {
+        assertEquals(1, day04a(listOf("aa bb cc dd aaa")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(325, day04a(readInputLines("day04", 2017)))
+    }
+}
+
+class Day04bTests {
+    @Test fun testExampleInput1() {
+        assertEquals(1, day04b(listOf("abcde fghij")))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(0, day04b(listOf("abcde xyz ecdab")))
+    }
+
+    @Test fun testExampleInput3() {
+        assertEquals(1, day04b(listOf("a ab abc abd abf abj")))
+    }
+
+    @Test fun testExampleInput4() {
+        assertEquals(1, day04b(listOf("iiii oiii ooii oooi oooo")))
+    }
+
+    @Test fun testExampleInput5() {
+        assertEquals(0, day04b(listOf("oiii ioii iioi iiio")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(119, day04b(readInputLines("day04", 2017)))
+    }
+}
