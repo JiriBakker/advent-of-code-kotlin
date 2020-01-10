aa94c900942ac452c9767ab8688e59ad98272e46
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-01-10 09:11:01 +0100

    Implemented 2017 Day 11

---
+++ src/test/kotlin/v2017/day11.kt
@@ -0,0 +1,33 @@
+package v2017.days.day11
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import util.readInputLine
+
+class Day11aTests {
+    @Test fun testExampleInput1() {
+        assertEquals(3, day11a("ne,ne,ne"))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(0, day11a("ne,ne,sw,sw"))
+    }
+
+    @Test fun testExampleInput3() {
+        assertEquals(2, day11a("ne,ne,s,s"))
+    }
+
+    @Test fun testExampleInput4() {
+        assertEquals(3, day11a("se,sw,se,sw,sw"))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(685, day11a(readInputLine("day11", 2017)))
+    }
+}
+
+class Day11bTests {
+    @Test fun testActualInput() {
+        assertEquals(1457, day11b(readInputLine("day11", 2017)))
+    }
+}
