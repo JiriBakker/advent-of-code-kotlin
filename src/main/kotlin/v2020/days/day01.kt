52c479a341a276acdffa8c31949d319bf2747df4
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-01 08:47:57 +0100

    Minor cleanup of Day 1

--- src/main/kotlin/v2020/days/day01.kt
+++ src/main/kotlin/v2020/days/day01.kt
@@ -1,27 +1,29 @@
 package v2020.days.day01
 
-private tailrec fun findMatchingPair(
-    target: Int,
-    nrs: List<Int>,
-    curIndex: Int,
-    seen: MutableSet<Int>
-): Pair<Int, Int>? {
-    if (curIndex >= nrs.size) {
-        return null
+private fun findMatchingPair(target: Int, nrs: List<Int>, startIndex: Int = 0): Pair<Int, Int>? {
+    val seen = mutableSetOf<Int>()
+
+    tailrec fun findRecursive(curIndex: Int): Pair<Int, Int>? {
+        if (curIndex >= nrs.size) {
+            return null
+        }
+
+        val toFind = target - nrs[curIndex]
+        if (seen.contains(toFind)) {
+            return toFind to nrs[curIndex]
+        }
+
+        seen.add(nrs[curIndex])
+
+        return findRecursive(curIndex + 1)
     }
 
-    val toFind = target - nrs[curIndex]
-    if (seen.contains(toFind)) {
-        return toFind to nrs[curIndex]
-    }
-
-    seen.add(nrs[curIndex])
-
-    return findMatchingPair(target, nrs, curIndex + 1, seen)
+    return findRecursive(startIndex)
 }
 
 fun day01a(input: List<String>): Int {
-    val match = findMatchingPair(2020, input.map(String::toInt), 0, mutableSetOf()) ?: error("No match found")
+    val nrs = input.map(String::toInt)
+    val match = findMatchingPair(2020, nrs, 0) ?: error("No match found")
     return match.first * match.second
 }
 
@@ -29,7 +31,7 @@ fun day01b(input: List<String>): Int {
     val nrs = input.map(String::toInt)
 
     nrs.forEachIndexed { index, nr ->
-        val match = findMatchingPair(2020 - nr, input.map(String::toInt), index + 1, mutableSetOf())
+        val match = findMatchingPair(2020 - nr, nrs, index + 1)
         if (match != null) {
             return match.first * match.second * nr
         }
