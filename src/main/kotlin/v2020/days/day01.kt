36c8e41ddb4a6e6dec9588907819112d1a485975
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-02 08:45:17 +0100

    Revert Day 1 back to simpler solution (which is 100% correct)

--- src/main/kotlin/v2020/days/day01.kt
+++ src/main/kotlin/v2020/days/day01.kt
@@ -1,65 +1,41 @@
 package v2020.days.day01
 
-import util.product
+private fun findMatchingPair(target: Int, nrs: List<Int>, startIndex: Int = 0): Pair<Int, Int>? {
+    val seen = mutableSetOf<Int>()
 
-private fun findMatch(target: Int, length: Int, nrs: List<Int>): List<Int> {
-    val seen = mutableMapOf(1 to nrs.associateWith { listOf(it) }.toMutableMap())
-
-    fun memoize(combination: List<Int>) {
-        seen
-            .getOrPut(combination.size, { mutableMapOf() })
-            .putIfAbsent(combination.sum(), combination)
-    }
-
-    fun getMemoized(sum: Int, length: Int): List<Int>? {
-        return seen[length]?.get(sum)
-    }
-
-    fun findMatchRecursive(remaining: Int, length: Int, curNrIndex: Int, used: List<Int>): List<Int>? {
-        val memoized = getMemoized(remaining, length)
-        if (memoized != null) {
-            return used.plus(memoized)
+    tailrec fun findRecursive(curIndex: Int): Pair<Int, Int>? {
+        if (curIndex >= nrs.size) {
+            return null
         }
 
-        if (length > 1) {
-            (curNrIndex until nrs.size).forEach { index ->
-                val nr = nrs[index]
-                val newUsed = used.plus(nr)
-
-                val match = findMatchRecursive(
-                    remaining - nr,
-                    length - 1,
-                    curNrIndex + 1,
-                    newUsed
-                )
-
-                if (match != null) {
-                    return match
-                }
-
-                memoize(newUsed)
-            }
+        val toFind = target - nrs[curIndex]
+        if (seen.contains(toFind)) {
+            return toFind to nrs[curIndex]
         }
 
-        return null
+        seen.add(nrs[curIndex])
+
+        return findRecursive(curIndex + 1)
     }
 
-    return findMatchRecursive(target, length, 0, emptyList()) ?: error("No match found")
+    return findRecursive(startIndex)
 }
 
 fun day01a(input: List<String>): Int {
     val nrs = input.map(String::toInt)
-
-    val match = findMatch(2020, 2, nrs)
-
-    return match.product()
+    val match = findMatchingPair(2020, nrs, 0) ?: error("No match found")
+    return match.first * match.second
 }
 
 fun day01b(input: List<String>): Int {
     val nrs = input.map(String::toInt)
 
-    val match = findMatch(2020, 3, nrs)
+    nrs.forEachIndexed { index, nr ->
+        val match = findMatchingPair(2020 - nr, nrs, index + 1)
+        if (match != null) {
+            return match.first * match.second * nr
+        }
+    }
 
-    return match.product()
+    error("No match found")
 }
-
--- src/test/kotlin/v2020/day01.kt
+++ src/test/kotlin/v2020/day01.kt
@@ -11,6 +11,10 @@ class Day01aTests {
         assertEquals(514579, day01a(listOf("1721","979","366","299","675","1456")))
     }
 
+    @Test fun testCustomInput1() {
+        assertEquals(1020000, day01a(listOf("1010","1000","1020")))
+    }
+
     @Test fun testActualInput() {
         assertEquals(651651, day01a(readInputLines("day01", 2020)))
     }
