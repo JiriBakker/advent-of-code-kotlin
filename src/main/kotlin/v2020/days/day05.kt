c015d9073c196b3779897dbb17c2242792775d16
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-05 09:16:58 +0100

    Refactored Day 5

--- src/main/kotlin/v2020/days/day05.kt
+++ src/main/kotlin/v2020/days/day05.kt
@@ -1,55 +1,27 @@
 package v2020.days.day05
 
-private fun getBoardingPasses(input: List<String>): List<Pair<Int, Int>> {
-    return input.map { line ->
-        var rowRange = 0 .. 127
-        line.take(7).forEach { char ->
-            val mid = rowRange.first + (rowRange.count() / 2)
-            if (char == 'B') {
-                rowRange = mid .. rowRange.last
-            } else {
-                rowRange = rowRange.first until mid
-            }
-        }
-
-        var colRange = 0 .. 7
-        line.takeLast(3).forEach { char ->
-            val mid = colRange.first + (colRange.count() / 2)
-            if (char == 'R') {
-                colRange = mid .. colRange.last
-            } else {
-                colRange = colRange.first until mid
-            }
-        }
-
-        rowRange.first() to colRange.first()
-    }
+private fun String.toSeatId(): Int {
+    return Integer.parseInt(
+            this.replace('F', '0')
+                .replace('B', '1')
+                .replace('L', '0')
+                .replace('R', '1')
+        , 2)
 }
 
 fun day05a(input: List<String>): Int {
-    return getBoardingPasses(input).map { it.first * 8 + it.second }.max()!!
+    return input.map(String::toSeatId).max()!!
 }
 
 fun day05b(input: List<String>): Int {
-    val boardingPasses = getBoardingPasses(input)
+    val seatIds = input.map(String::toSeatId).sorted()
 
-    val expectedCols = 0 .. 7
+    for (i in seatIds.indices) {
+        if (seatIds[i] != seatIds[i + 1] - 1) {
+            return seatIds[i] + 1
+        }
+    }
 
-    val actualRow =
-        boardingPasses
-            .groupBy({ it.first }) { it.second }
-            .entries
-
-            // Ignore full rows
-            .filter { it.value.size < 8 }
-
-            // Ignore front row
-            .sortedBy { it.key }.drop(1)
-
-            .first()
-
-    val missingCol = expectedCols.minus(actualRow.value).first()
-
-    return actualRow.key * 8 + missingCol
+    error("No empty seat found")
 }
 
