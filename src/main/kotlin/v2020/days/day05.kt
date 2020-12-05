e2f3942dc9f34515a5cb72951d35f6e3453a5b49
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-05 09:22:05 +0100

    Refactored Day 5 a bit further

--- src/main/kotlin/v2020/days/day05.kt
+++ src/main/kotlin/v2020/days/day05.kt
@@ -16,12 +16,9 @@ fun day05a(input: List<String>): Int {
 fun day05b(input: List<String>): Int {
     val seatIds = input.map(String::toSeatId).sorted()
 
-    for (i in seatIds.indices) {
-        if (seatIds[i] != seatIds[i + 1] - 1) {
-            return seatIds[i] + 1
-        }
-    }
-
-    error("No empty seat found")
+    return seatIds
+        .zipWithNext()
+        .first { (cur, next) -> cur != next - 1 }
+        .let { it.first + 1 }
 }
 
