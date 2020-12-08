79957525481ae6d31f3d1a9d8f758fe7fe5d9e4e
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-08 06:55:24 +0100

    Implemented Day 8 (2020)

--- src/main/kotlin/v2020/days/day08.kt
+++ src/main/kotlin/v2020/days/day08.kt
@@ -1,10 +1,57 @@
 package v2020.days.day08
 
+private fun runProgram(input: List<String>): Pair<Boolean, Int> {
+    val instructions = input.map { line -> line.split(" ").map { it.trimEnd(',') } }
+
+    val seen = mutableSetOf<Int>()
+    var accumulator = 0
+    var pointer = 0
+
+    while (true) {
+        if (!seen.add(pointer)) {
+            return false to accumulator
+        }
+
+        if (pointer !in instructions.indices) {
+            return true to accumulator
+        }
+
+        val instruction = instructions[pointer]
+        when (instruction[0]) {
+            "nop" -> pointer++
+            "acc" -> {
+                accumulator += instruction[1].toInt()
+                pointer++
+            }
+            "jmp" -> pointer += instruction[1].toInt()
+            else -> error("Unknown instruction")
+        }
+    }
+}
+
+private fun swapNopJmp(instruction: String): String {
+    return instruction
+        .replace("nop", "tmp")
+        .replace("jmp", "nop")
+        .replace("tmp", "jmp")
+}
+
 fun day08a(input: List<String>): Int {
-    return 0
+    return runProgram(input).second
 }
 
 fun day08b(input: List<String>): Int {
-    return 0
+    input.mapIndexed { index, line ->
+        if (line.startsWith("nop") || line.startsWith("jmp")) {
+            val inputCopy = input.toMutableList()
+            inputCopy[index] = swapNopJmp(inputCopy[index])
+            val (terminated, accumulator) = runProgram(inputCopy)
+            if (terminated) {
+                return accumulator
+            }
+        }
+    }
+
+    error("Program never terminated")
 }
 
--- src/test/kotlin/v2020/day08.kt
+++ src/test/kotlin/v2020/day08.kt
@@ -7,23 +7,39 @@ import v2020.days.day08.day08b
 import util.readInputLines
 
 class Day08aTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day08a(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day08a(readInputLines("day08", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(5, day08a(listOf(
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(2034, day08a(readInputLines("day08", 2020)))
+    }
 }
 
 class Day08bTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day08b(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day08b(readInputLines("day08", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(8, day08b(listOf(
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(672, day08b(readInputLines("day08", 2020)))
+    }
 }
