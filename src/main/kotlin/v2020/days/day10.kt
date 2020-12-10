09bb18d419f4c29c87c13e3eebcace5f447fc5fb
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-10 09:19:48 +0100

    Implemented Day 10 (2020)

--- src/main/kotlin/v2020/days/day10.kt
+++ src/main/kotlin/v2020/days/day10.kt
@@ -1,10 +1,36 @@
 package v2020.days.day10
 
-fun day10a(input: List<String>): Int {
-    return 0
+private fun getJoltDiffs(input: List<String>): List<Int> {
+    val adapters = input
+        .map(String::toInt)
+        .sorted()
+
+    return listOf(0).plus(adapters).plus(adapters.max()!! + 3)
+        .zipWithNext { prev, next ->
+            next - prev
+        }
 }
 
-fun day10b(input: List<String>): Int {
-    return 0
+fun day10a(input: List<String>): Int {
+    val joltDiffs = getJoltDiffs(input)
+
+    return joltDiffs.count { it == 1 } * joltDiffs.count { it == 3 }
+}
+
+fun day10b(input: List<String>): Long {
+    val joltDiffs = getJoltDiffs(input)
+
+    return joltDiffs.fold(Pair(1L, 0)) {(comboCount, seqOfOnesLength), joltDiff ->
+        if (joltDiff == 1) {
+            comboCount to seqOfOnesLength + 1
+        } else {
+            when (seqOfOnesLength) {
+                4 -> comboCount * 7 to 0
+                3 -> comboCount * 4 to 0
+                2 -> comboCount * 2 to 0
+                else -> comboCount to 0
+            }
+        }
+    }.first
 }
 
--- src/test/kotlin/v2020/day10.kt
+++ src/test/kotlin/v2020/day10.kt
@@ -7,23 +7,113 @@ import v2020.days.day10.day10b
 import util.readInputLines
 
 class Day10aTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day10a(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day10a(readInputLines("day10", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(35, day10a(listOf(
+            "16",
+            "10",
+            "15",
+            "5",
+            "1",
+            "11",
+            "7",
+            "19",
+            "6",
+            "12",
+            "4")))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(220, day10a(listOf(
+            "28",
+            "33",
+            "18",
+            "42",
+            "31",
+            "14",
+            "46",
+            "20",
+            "48",
+            "47",
+            "24",
+            "23",
+            "49",
+            "45",
+            "19",
+            "38",
+            "39",
+            "11",
+            "1",
+            "32",
+            "25",
+            "35",
+            "8",
+            "17",
+            "7",
+            "9",
+            "4",
+            "2",
+            "34",
+            "10",
+            "3")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(2240, day10a(readInputLines("day10", 2020)))
+    }
 }
 
 class Day10bTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day10b(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day10b(readInputLines("day10", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(8, day10b(listOf(
+            "16",
+            "10",
+            "15",
+            "5",
+            "1",
+            "11",
+            "7",
+            "19",
+            "6",
+            "12",
+            "4")))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(19208, day10b(listOf(
+            "28",
+            "33",
+            "18",
+            "42",
+            "31",
+            "14",
+            "46",
+            "20",
+            "48",
+            "47",
+            "24",
+            "23",
+            "49",
+            "45",
+            "19",
+            "38",
+            "39",
+            "11",
+            "1",
+            "32",
+            "25",
+            "35",
+            "8",
+            "17",
+            "7",
+            "9",
+            "4",
+            "2",
+            "34",
+            "10",
+            "3")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(99214346656768L, day10b(readInputLines("day10", 2020)))
+    }
 }
