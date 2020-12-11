a8566e79616f7f2562a8ac2a44b2ae4355657bd5
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-11 07:38:34 +0100

    Implemented Day 11 (2020)

--- src/main/kotlin/v2020/days/day11.kt
+++ src/main/kotlin/v2020/days/day11.kt
@@ -1,10 +1,88 @@
 package v2020.days.day11
 
+private fun printGrid(grid: List<List<Char>>) {
+    for (y in grid.indices) {
+        for (x in grid[y].indices) {
+            print(grid[y][x])
+        }
+        println()
+    }
+}
+
+private fun iterateGrid(
+    grid: List<List<Char>>,
+    useDeepSearch: Boolean = false,
+    filledSeatsLimit: Int = 4
+): Pair<List<List<Char>>, Boolean> {
+    val nextGrid = grid.map { it.toMutableList() }
+    var hasChanged = false
+
+    fun getNeighbours(x: Int, y: Int): List<Char> {
+        fun deepSearchNeighbour(xDelta: Int, yDelta: Int, steps: Int = 1): Char? {
+            val neighbour = grid.getOrNull(y + yDelta * steps)?.getOrNull(x + xDelta * steps)
+            if (useDeepSearch && neighbour == '.') {
+                return deepSearchNeighbour(xDelta, yDelta, steps + 1)
+            }
+            return neighbour
+        }
+
+        return listOfNotNull(
+            deepSearchNeighbour(-1, -1),
+            deepSearchNeighbour(0, -1),
+            deepSearchNeighbour(1, -1),
+            deepSearchNeighbour(-1, 0),
+            deepSearchNeighbour(1, 0),
+            deepSearchNeighbour(-1, 1),
+            deepSearchNeighbour(0, 1),
+            deepSearchNeighbour(1, 1)
+        )
+    }
+
+    for (y in grid.indices) {
+        for (x in grid[y].indices) {
+            val cell = grid[y][x]
+            if (cell == '.') {
+                nextGrid[y][x] = '.'
+            } else {
+                val neighbours = getNeighbours(x, y)
+                if (grid[y][x] == 'L' && neighbours.none { it == '#' }) {
+                    nextGrid[y][x] = '#'
+                    hasChanged = true
+                } else if (grid[y][x] == '#' && neighbours.count { it == '#' } >= filledSeatsLimit){
+                    nextGrid[y][x] = 'L'
+                    hasChanged = true
+                }
+                else {
+                    nextGrid[y][x] = grid[y][x]
+                }
+            }
+        }
+    }
+
+    return nextGrid to hasChanged
+}
+
 fun day11a(input: List<String>): Int {
-    return 0
+    var grid = input.map { it.toCharArray().toList() }
+
+    while (true) {
+        val (nextGrid, hasChanged) = iterateGrid(grid)
+        if (!hasChanged) {
+            return nextGrid.sumBy { row -> row.count { it == '#' } }
+        }
+        grid = nextGrid
+    }
 }
 
 fun day11b(input: List<String>): Int {
-    return 0
+    var grid = input.map { it.toCharArray().toList() }
+
+    while (true) {
+        val (nextGrid, hasChanged) = iterateGrid(grid, true, 5)
+        if (!hasChanged) {
+            return nextGrid.sumBy { row -> row.count { it == '#' } }
+        }
+        grid = nextGrid
+    }
 }
 
--- src/test/kotlin/v2020/day11.kt
+++ src/test/kotlin/v2020/day11.kt
@@ -7,23 +7,41 @@ import v2020.days.day11.day11b
 import util.readInputLines
 
 class Day11aTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day11a(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day11a(readInputLines("day11", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(37, day11a(listOf(
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(2427, day11a(readInputLines("day11", 2020)))
+    }
 }
 
 class Day11bTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day11b(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day11b(readInputLines("day11", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(26, day11b(listOf(
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(2199, day11b(readInputLines("day11", 2020)))
+    }
 }
