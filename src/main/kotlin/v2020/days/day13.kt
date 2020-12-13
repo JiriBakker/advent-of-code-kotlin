9e8990cdabb857d178bb21b4a23390e78ce2954d
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-13 10:19:02 +0100

    Minor refactoring day13

--- src/main/kotlin/v2020/days/day13.kt
+++ src/main/kotlin/v2020/days/day13.kt
@@ -16,16 +16,16 @@ fun day13a(input: List<String>): Long {
         .let { computeTimeToWait(it) * it }
 }
 
-private class Bus(val interval: Long, val offset: Long)
+private class BusCycle(val interval: Long, val offset: Long)
 
-private fun findAlignmentTime(bus1: Bus, bus2: Bus): Bus {
-    var time = bus1.offset
+private fun findAlignmentTime(busCycle1: BusCycle, busCycle2: BusCycle): BusCycle {
+    var time = busCycle1.offset
     while (time < Long.MAX_VALUE) {
-        val busesAlign = (time + bus2.offset).safeMod(bus2.interval) == 0L
-        if (busesAlign) {
-            return Bus(leastCommonMultiple(bus1.interval, bus2.interval), time)
+        val doBusCyclesAlign = (time + busCycle2.offset).safeMod(busCycle2.interval) == 0L
+        if (doBusCyclesAlign) {
+            return BusCycle(leastCommonMultiple(busCycle1.interval, busCycle2.interval), time)
         }
-        time += bus1.interval
+        time += busCycle1.interval
     }
     error("Buses never align (or takes too long)")
 }
@@ -33,7 +33,7 @@ private fun findAlignmentTime(bus1: Bus, bus2: Bus): Bus {
 fun day13b(input: List<String>): Long {
     val buses = input[1]
         .split(",")
-        .mapIndexed { index, busNr -> if (busNr == "x") null else Bus(busNr.toLong(), index.toLong()) }
+        .mapIndexed { index, busNr -> if (busNr == "x") null else BusCycle(busNr.toLong(), index.toLong()) }
         .filterNotNull()
 
     return buses.reduce { acc, bus -> findAlignmentTime(acc, bus) }.offset
