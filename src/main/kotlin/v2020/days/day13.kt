438ef1ec9da45a7aed427000cd15559f72c1eb90
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2020-12-13 08:46:41 +0100

    Implemented Day 13 (2020)

--- src/main/kotlin/v2020/days/day13.kt
+++ src/main/kotlin/v2020/days/day13.kt
@@ -1,10 +1,39 @@
 package v2020.days.day13
 
-fun day13a(input: List<String>): Int {
-    return 0
+import util.leastCommonMultiple
+import util.safeMod
+
+fun day13a(input: List<String>): Long {
+    val departTime = input[0].toLong()
+    val busNrs = input[1].replace("x,", "").split(",").map { it.toLong() }
+
+    fun computeTimeToWait(busNr: Long): Long {
+        return busNr - departTime % busNr
+    }
+
+    return busNrs.minBy { computeTimeToWait(it) }!!.let { computeTimeToWait(it) * it }
 }
 
-fun day13b(input: List<String>): Int {
-    return 0
+private class Bus(val interval: Long, val offset: Long)
+
+private fun findAlignmentTime(bus1: Bus, bus2: Bus): Bus {
+    var time = bus1.offset
+    while (time < Long.MAX_VALUE) {
+        if ((time + bus2.offset).safeMod(bus2.interval) == 0L) {
+            return Bus(leastCommonMultiple(bus1.interval, bus2.interval), time)
+        }
+        time += bus1.interval
+    }
+    error("Buses never align (or takes too long)")
+}
+
+fun day13b(input: List<String>): Long {
+    val buses = input[1]
+        .split(",")
+        .mapIndexed { index, busNr -> if (busNr == "x") null else Bus(busNr.toLong(), index.toLong()) }
+        .filterNotNull()
+
+
+    return buses.reduce { acc, bus -> findAlignmentTime(acc, bus) }.offset
 }
 
--- src/test/kotlin/v2020/day13.kt
+++ src/test/kotlin/v2020/day13.kt
@@ -7,23 +7,61 @@ import v2020.days.day13.day13b
 import util.readInputLines
 
 class Day13aTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day13a(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day13a(readInputLines("day13", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(295, day13a(listOf(
+            "939",
+            "7,13,x,x,59,x,31,19")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(138, day13a(readInputLines("day13", 2020)))
+    }
 }
 
 class Day13bTests {
-    // @Test fun testExampleInput1() {
-    //     assertEquals(0, day13b(listOf(
-    //         "")))
-    // }
-    //
-    // @Test fun testActualInput() {
-    //     assertEquals(0, day13b(readInputLines("day13", 2020)))
-    // }
+    @Test fun testExampleInput1() {
+        assertEquals(1068781, day13b(listOf(
+            "0",
+            "7,13,x,x,59,x,31,19")))
+    }
+
+    @Test fun testExampleInput2() {
+        assertEquals(3417, day13b(listOf(
+            "0",
+            "17,x,13,19")))
+    }
+
+    @Test fun testExampleInput3() {
+        assertEquals(754018, day13b(listOf(
+            "0",
+            "67,7,59,61")))
+    }
+
+    @Test fun testExampleInput4() {
+        assertEquals(779210, day13b(listOf(
+            "0",
+            "67,x,7,59,61")))
+    }
+
+    @Test fun testExampleInput5() {
+        assertEquals(1261476, day13b(listOf(
+            "0",
+            "67,7,x,59,61")))
+    }
+
+    @Test fun testExampleInput6() {
+        assertEquals(1202161486, day13b(listOf(
+            "0",
+            "1789,37,47,1889")))
+    }
+
+    @Test fun testCustomInput1() {
+        assertEquals(2, day13b(listOf(
+            "0",
+            "2,3,4")))
+    }
+
+    @Test fun testActualInput() {
+        assertEquals(226845233210288L, day13b(readInputLines("day13", 2020)))
+    }
 }
