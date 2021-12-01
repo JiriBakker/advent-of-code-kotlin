b595defe541b53823f4e6208e46b734ec4db7bb6
Jiri Bakker <jiribakker@bol.com>
2021-12-01 08:21:18 +0100

    Implemented Day 1 (2021)

--- src/test/kotlin/v2021/day01.kt
+++ src/test/kotlin/v2021/day01.kt
@@ -7,28 +7,43 @@ import util.readInputLines
 
 class Day01aTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day01a(listOf()))
+        assertEquals(7, day01a(listOf(
+            "199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263")))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day01a(readInputLines("day01", 2021)))
+        assertEquals(1722, day01a(readInputLines("day01", 2021)))
     }
 }
 
 class Day01bTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day01b(listOf()))
+        assertEquals(5, day01b(listOf("199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263")))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day01b(readInputLines("day01", 2021)))
+        assertEquals(1748, day01b(readInputLines("day01", 2021)))
     }
 }
