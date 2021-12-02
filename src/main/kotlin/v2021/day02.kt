7dfc08aba91635878b6ccef6c957b5132268a959
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2021-12-02 07:19:05 +0100

    Implemented Day 2 (2021)

--- src/main/kotlin/v2021/day02.kt
+++ src/main/kotlin/v2021/day02.kt
@@ -1,13 +1,47 @@
 package v2021
 
-import util.DoNotAutoExecute
-
-@DoNotAutoExecute
-fun day02a(input: List<String>): Int {
-    return 0
+private fun List<String>.parseMovements(): List<Pair<String, Int>> {
+    return map { line ->
+        val (direction, amount) = line.split(" ")
+        direction to amount.toInt()
+    }
 }
 
-@DoNotAutoExecute
+fun day02a(input: List<String>): Int {
+    var x = 0
+    var y = 0
+
+    input.parseMovements().forEach { (direction, amount) ->
+        when (direction) {
+            "forward" -> x += amount
+            "down" -> y += amount
+            "up" -> y -= amount
+            else -> throw Error("Unknown direction: $direction")
+        }
+    }
+    return x * y
+}
+
 fun day02b(input: List<String>): Int {
-    return 0
+    var x = 0
+    var y = 0
+    var aim = 0
+
+    input.forEach { movement ->
+        val (direction, amount) = movement.split(" ")
+        when (direction) {
+            "forward" -> {
+                x += amount.toInt()
+                y += aim * amount.toInt()
+            }
+            "down" -> {
+                aim += amount.toInt()
+            }
+            "up" -> {
+                aim -= amount.toInt()
+            }
+            else -> throw Error("Unknown direction: $direction")
+        }
+    }
+    return x * y
 }
