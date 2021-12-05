142159fd157dc2af4676c7e650311e109213e228
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2021-12-05 10:13:05 +0100

    Rewrote Day 4 in a more OO approach

--- src/main/kotlin/v2021/day04.kt
+++ src/main/kotlin/v2021/day04.kt
@@ -1,8 +1,39 @@
 package v2021
 
+import util.sumOf
 import v2021.Day04.parse
 
-private typealias BingoCard = List<List<Int>>
+private class BingoCard(numbers: List<List<Int>>) {
+    private val rows: List<MutableMap<Int, Boolean>>
+    private val cols: List<MutableMap<Int, Boolean>>
+
+    init {
+        rows = numbers.map { row -> row.associateWith { false }.toMutableMap() }
+        cols = numbers[0].mapIndexed { columnIndex, _ ->
+            numbers.associate { row -> row[columnIndex] to false }.toMutableMap()
+        }
+    }
+
+    private fun MutableMap<Int, Boolean>.markSafe(nr: Int) {
+        if (containsKey(nr)) {
+            this[nr] = true
+        }
+    }
+
+    fun mark(nr: Int) {
+        rows.forEach { row -> row.markSafe(nr) }
+        cols.forEach { col -> col.markSafe(nr) }
+    }
+
+    private fun Map<Int, Boolean>.allMarked() =
+        all { (_, isMarked) -> isMarked }
+
+    val hasBingo get() =
+        rows.any { row -> row.allMarked() } || cols.any { col -> col.allMarked() }
+
+    val score get() =
+        rows.sumOf { row -> row.sumOf { (nr, isMarked) -> if (isMarked) 0 else nr } }
+}
 
 private object Day04 {
     fun List<String>.parse(): Pair<List<Int>, List<BingoCard>> {
@@ -22,74 +53,48 @@ private object Day04 {
                                 .map { it.toInt() }
                         }
                 }
+                .map { BingoCard(it) }
 
         return nrsToDraw to bingoCards
     }
 }
 
-private fun List<BingoCard>.getWinningLines(): Map<Set<Int>, BingoCard> {
-    return this.flatMap { bingoCard ->
-        buildList {
-            addAll(bingoCard)
-            addAll((0 .. 4).map { column -> bingoCard.map { row -> row[column] } })
-        }
-            .map { it.toSet() to bingoCard }
-    }.toMap()
-}
-
 private fun drawNrs(
     nrsToDraw: List<Int>,
-    winningLines: Map<Set<Int>, BingoCard>,
-    bingoCardCompletedCallback: (BingoCard, List<Int>) -> Int?
+    bingoCards: List<BingoCard>,
+    bingoCardCompletedCallback: (BingoCard, Int) -> Int?
 ): Int {
-    val hitsPerLine = winningLines.keys.associateWith { 0 }.toMutableMap()
-    val nrsDrawn = mutableListOf<Int>()
-
     nrsToDraw.forEach { nr ->
-        nrsDrawn.add(nr)
+        bingoCards.forEach { bingoCard ->
+            bingoCard.mark(nr)
 
-        winningLines
-            .filter { it.key.contains(nr) } // Only lines that have the drawn nr
-            .forEach { (line, bingoCard) ->
-                hitsPerLine[line] = hitsPerLine[line]!! + line.count { it == nr }
-                if (hitsPerLine[line] == 5) { // Line completed
-                    bingoCardCompletedCallback
-                        .invoke(bingoCard, nrsDrawn)
-                        ?.let { return it } // If callback returns something, answer is found, so return it
-                }
+            if (bingoCard.hasBingo) {
+                bingoCardCompletedCallback.invoke(bingoCard, nr)
+                    ?.let { return it } // If callback returns something, answer is found, so return it
             }
+        }
     }
 
     throw Error("No winning bingo card")
 }
 
-private fun computeFinalScore(completedBingoCard: BingoCard, nrsDrawn: List<Int>) =
-    completedBingoCard
-        .sumOf { row ->
-            row
-                .filter { nr -> !nrsDrawn.contains(nr) } // TODO List.contains not super efficient
-                .sum()
-        } * nrsDrawn.last()
-
 fun day04a(input: List<String>): Int {
     val (nrsToDraw, bingoCards) = input.parse()
-    val winningLines = bingoCards.getWinningLines()
 
-    return drawNrs(nrsToDraw, winningLines) { completedBingoCard, nrsDrawn ->
-        computeFinalScore(completedBingoCard, nrsDrawn)
+    return drawNrs(nrsToDraw, bingoCards) { completedBingoCard, lastNr ->
+        completedBingoCard.score * lastNr
     }
 }
 
 fun day04b(input: List<String>): Int {
     val (nrsToDraw, bingoCards) = input.parse()
-    val winningLines = bingoCards.getWinningLines()
 
-    val bingoCardsCompleted = mutableSetOf<List<List<Int>>>()
+    val bingoCardsCompleted = mutableSetOf<BingoCard>()
 
-    return drawNrs(nrsToDraw, winningLines) { completedBingoCard, nrsDrawn ->
+    return drawNrs(nrsToDraw, bingoCards) { completedBingoCard, lastNr ->
         bingoCardsCompleted.add(completedBingoCard)
         if (bingoCardsCompleted.count() == bingoCards.count()) { // All bingo cards completed
-            computeFinalScore(completedBingoCard, nrsDrawn)
+            completedBingoCard.score * lastNr
         } else null
     }
 }
