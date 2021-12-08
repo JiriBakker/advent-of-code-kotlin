f928161bd3a20f9c5159aaa53e7650ec4c8f5095
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2021-12-08 08:00:47 +0100

    Implemented Day 8 (2021)

--- src/main/kotlin/v2021/day08.kt
+++ src/main/kotlin/v2021/day08.kt
@@ -1,13 +1,89 @@
 package v2021
 
-import util.DoNotAutoExecute
+import util.sorted
 
-@DoNotAutoExecute
 fun day08a(input: List<String>): Int {
-    return 0
+    val outputs = input.map { it.split(" | ")[1].split(" ") }
+    return outputs.sumOf { output -> output.count{ listOf(2,3,4,7).contains(it.length) } }
 }
 
-@DoNotAutoExecute
+private fun determineDigitSignals(signals: List<String>): Map<String, Int> {
+    fun String.countMatches(other: String) = count { other.contains(it) }
+
+    val one   = signals.first { it.length == 2 }
+    val seven = signals.first { it.length == 3 }
+    val four  = signals.first { it.length == 4 }
+    val eight = signals.first { it.length == 7 }
+
+    val six = signals.first { signal ->
+        signal.length == 6
+            && one.countMatches(signal) == 1
+    }
+
+    val zero = signals.first { signal ->
+        signal.length == 6
+            && one.countMatches(signal) == 2
+            && four.countMatches(signal) == 3
+            && seven.countMatches(signal) == 3
+    }
+
+
+    val three = signals.first { signal ->
+        signal.length == 5
+            && one.countMatches(signal) == 2
+            && four.countMatches(signal) == 3
+            && seven.countMatches(signal) == 3
+            && six.countMatches(signal) == 4
+    }
+
+    val five = signals.first { signal ->
+        signal.length == 5
+            && one.countMatches(signal) == 1
+            && four.countMatches(signal) == 3
+            && seven.countMatches(signal) == 2
+            && six.countMatches(signal) == 5
+    }
+
+    val nine = signals.first { signal ->
+        signal.length == 6
+            && one.countMatches(signal) == 2
+            && four.countMatches(signal) == 4
+            && seven.countMatches(signal) == 3
+            && zero.countMatches(signal) == 5
+    }
+
+     val two = signals.first { signal ->
+        signal.length == 5
+            && one.count   { signal.contains(it) } == 1
+            && four.count  { signal.contains(it) } == 2
+            && seven.countMatches(signal) == 2
+            && six.count   { signal.contains(it) } == 4
+    }
+
+    return mapOf(
+        zero.sorted()  to 0,
+        one.sorted()   to 1,
+        two.sorted()   to 2,
+        three.sorted() to 3,
+        four.sorted()  to 4,
+        five.sorted()  to 5,
+        six.sorted()   to 6,
+        seven.sorted() to 7,
+        eight.sorted() to 8,
+        nine.sorted()  to 9
+    )
+}
+
 fun day08b(input: List<String>): Int {
-    return 0
+    return input
+        .map { it.split(" | ") }
+        .sumOf { (signals, output) ->
+            val digitSignals = determineDigitSignals(signals.split(" "))
+            val outputSignals = output.split(" ").map { it.sorted() }
+
+            digitSignals[outputSignals[0]]!! * 1000 +
+                digitSignals[outputSignals[1]]!! * 100 +
+                digitSignals[outputSignals[2]]!! * 10 +
+                digitSignals[outputSignals[3]]!!
+        }
 }
