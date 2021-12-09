9691450c4f560d4559b2fce39dcd2f26543eaede
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2021-12-09 09:54:17 +0100

    Implemented Day 9 (2021)

--- src/main/kotlin/v2021/day09.kt
+++ src/main/kotlin/v2021/day09.kt
@@ -1,13 +1,76 @@
 package v2021
 
-import util.DoNotAutoExecute
+import util.product
 
-@DoNotAutoExecute
-fun day09a(input: List<String>): Int {
-    return 0
+private fun List<List<Int>>.getNeighbourPositions(x: Int, y: Int) =
+    listOfNotNull(
+        if (x > 0)                x - 1 to y else null,
+        if (x < this[0].size - 1) x + 1 to y else null,
+        if (y > 0)                x to y - 1 else null,
+        if (y < this.size - 1)    x to y + 1 else null
+    )
+
+private fun List<List<Int>>.findLowPoints(): List<Pair<Int, Int>> {
+    val grid = this
+
+    fun List<Pair<Int, Int>>.allHigherThan(value: Int) =
+        all { (x, y) -> grid[y][x] > value }
+
+    return flatMapIndexed { y, row ->
+        row.mapIndexedNotNull { x, value ->
+            val neighbours = getNeighbourPositions(x, y)
+            if (neighbours.allHigherThan(value))
+                x to y
+            else
+                null
+        }
+    }
 }
 
-@DoNotAutoExecute
+fun day09a(input: List<String>): Int {
+    val grid = input.map { row -> row.map { it.toString().toInt() } }
+
+    val lowPoints = grid.findLowPoints()
+
+    return lowPoints.sumOf { (x, y) -> grid[y][x] + 1 }
+}
+
 fun day09b(input: List<String>): Int {
-    return 0
+    val grid = input.map { row -> row.map { it.toString().toInt() } }
+
+    val lowPoints = grid.findLowPoints()
+
+    fun getBasinSize(x: Int, y: Int): Int {
+        val checked = mutableSetOf<Pair<Int, Int>>()
+        val toCheck = ArrayDeque<Pair<Int, Int>>()
+
+        toCheck.add(x to y)
+        var size = 0
+
+        while (toCheck.isNotEmpty()) {
+            val curPos = toCheck.removeFirst()
+
+            val curX = curPos.first
+            val curY = curPos.second
+
+            if (grid[curY][curX] == 9) continue
+            if (checked.contains(curPos)) continue
+
+            checked.add(curPos)
+            size++
+
+            toCheck.addAll(
+                grid.getNeighbourPositions(curX, curY)
+            )
+        }
+
+        return size
+    }
+
+    val basinSizes = lowPoints.map { (x, y) -> getBasinSize(x, y) }
+
+    return basinSizes
+        .sortedDescending()
+        .take(3)
+        .product()
 }
--- src/test/kotlin/v2021/day09.kt
+++ src/test/kotlin/v2021/day09.kt
@@ -1,34 +1,41 @@
 package v2021
 
 import org.junit.Assert.assertEquals
-import org.junit.Ignore
 import org.junit.Test
 import util.readInputLines
 
 class Day09aTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day09a(listOf()))
+        assertEquals(15, day09a(listOf(
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day09a(readInputLines("day09", 2021)))
+        assertEquals(439, day09a(readInputLines("day09", 2021)))
     }
 }
 
 class Day09bTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day09b(listOf()))
+        assertEquals(1134, day09b(listOf(
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day09b(readInputLines("day09", 2021)))
+        assertEquals(900900, day09b(readInputLines("day09", 2021)))
     }
 }
