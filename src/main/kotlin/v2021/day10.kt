1129acd4e89f59aa08754081bb957bc2fb9c065b
Jiri Bakker <jiribakker@bol.com>
2021-12-10 08:14:28 +0100

    Implemented Day 10 (2021)

--- src/main/kotlin/v2021/day10.kt
+++ src/main/kotlin/v2021/day10.kt
@@ -1,13 +1,90 @@
 package v2021
 
-import util.DoNotAutoExecute
+private val openingChars = setOf('[', '(', '{', '<')
 
-@DoNotAutoExecute
-fun day10a(input: List<String>): Int {
-    return 0
+private val opposingChars =
+    mapOf(
+        '(' to ')',
+        '[' to ']',
+        '{' to '}',
+        '<' to '>'
+    )
+
+private val illegalCharScores =
+    mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+   )
+
+private val incompleteCharScores =
+    mapOf(
+        '(' to 1,
+        '[' to 2,
+        '{' to 3,
+        '<' to 4
+    )
+
+private class CorruptedInputException(val corruptedChar: Char) : Exception()
+
+private fun String.findIncompleteChars(): List<Char> {
+    val openChars = ArrayDeque<Char>()
+
+    fun String.validate(startPos: Int, nextValidClosingChar: Char): Int {
+        var pos = startPos
+        while (pos < this.length) {
+            val char = this[pos]
+            when {
+                openingChars.contains(char) -> {
+                    openChars.addFirst(this[pos])
+                    pos = validate(pos + 1, opposingChars[char]!!)
+                }
+                char != nextValidClosingChar -> {
+                    throw CorruptedInputException(char)
+                }
+                else -> {
+                    openChars.removeFirst()
+                    return pos + 1
+                }
+            }
+        }
+        return pos
+    }
+
+    validate(0, ' ')
+
+    return openChars
 }
 
-@DoNotAutoExecute
-fun day10b(input: List<String>): Int {
-    return 0
+fun day10a(input: List<String>): Int {
+    return input.sumOf { line ->
+        try {
+            line.findIncompleteChars()
+            0
+        } catch (e: CorruptedInputException) {
+            illegalCharScores[e.corruptedChar]!!
+        }
+    }
+}
+
+private fun List<Char>.computeIncompleteCharsScore() =
+    map { incompleteCharScores[it]!!}
+        .fold(0L) { total, score -> total * 5L + score }
+
+private fun <T : Comparable<T>> List<T>.getMiddle() =
+    sorted()[size / 2]
+
+fun day10b(input: List<String>): Long {
+    val scores = input.mapNotNull { line ->
+        try {
+            line
+                .findIncompleteChars()
+                .computeIncompleteCharsScore()
+        } catch (e: CorruptedInputException) {
+            null
+        }
+    }
+
+    return scores.getMiddle()
 }
--- src/test/kotlin/v2021/day10.kt
+++ src/test/kotlin/v2021/day10.kt
@@ -1,34 +1,51 @@
 package v2021
 
 import org.junit.Assert.assertEquals
-import org.junit.Ignore
 import org.junit.Test
 import util.readInputLines
 
 class Day10aTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day10a(listOf()))
+        assertEquals(26397, day10a(listOf(
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day10a(readInputLines("day10", 2021)))
+        assertEquals(323613, day10a(readInputLines("day10", 2021)))
     }
 }
 
 class Day10bTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day10b(listOf()))
+        assertEquals(288957, day10b(listOf(
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day10b(readInputLines("day10", 2021)))
+        assertEquals(3103006161, day10b(readInputLines("day10", 2021)))
     }
 }
