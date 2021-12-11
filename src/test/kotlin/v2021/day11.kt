f64702ac48abda990e425ee120a6cc10d16d7972
Jiri Bakker <jiribakker@bol.com>
2021-12-11 08:11:56 +0100

    Implemented Day 11 (2021)

--- src/test/kotlin/v2021/day11.kt
+++ src/test/kotlin/v2021/day11.kt
@@ -7,28 +7,46 @@ import util.readInputLines
 
 class Day11aTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day11a(listOf()))
+        assertEquals(1656, day11a(listOf(
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day11a(readInputLines("day11", 2021)))
+        assertEquals(1665, day11a(readInputLines("day11", 2021)))
     }
 }
 
 class Day11bTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day11b(listOf()))
+        assertEquals(195, day11b(listOf(
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day11b(readInputLines("day11", 2021)))
+        assertEquals(235, day11b(readInputLines("day11", 2021)))
     }
 }
