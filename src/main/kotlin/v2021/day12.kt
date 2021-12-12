dd27e66abffcbae8e9aa5c359890a14bb00ebbc6
Jiri Bakker <jiribakker@bol.com>
2021-12-12 08:18:31 +0100

    Implemented Day 12 (2021)

--- src/main/kotlin/v2021/day12.kt
+++ src/main/kotlin/v2021/day12.kt
@@ -1,13 +1,57 @@
 package v2021
 
-import util.DoNotAutoExecute
+private data class Node(val id: String) {
+    val neighbours = mutableMapOf<String, Node>()
 
-@DoNotAutoExecute
-fun day12a(input: List<String>): Int {
-    return 0
+    val isSmallNode = id.all { it.isLowerCase() }
 }
 
-@DoNotAutoExecute
+private fun Node.explore(pathSoFar: List<String>, doubleVisitsRemaining: Int = 0): List<List<String>> {
+    return neighbours.flatMap { (id, node) ->
+        if (id == "end") {
+            listOf(pathSoFar.plus(id).toList())
+        } else if (id == "start") {
+            emptyList()
+        } else if (node.isSmallNode) {
+            val visitCount = pathSoFar.count { it == id }
+            if (visitCount == 0) {
+                node.explore(pathSoFar.plus(id), doubleVisitsRemaining)
+            } else if (visitCount == 1 && doubleVisitsRemaining > 0) {
+                node.explore(pathSoFar.plus(id), doubleVisitsRemaining - 1)
+            } else {
+                emptyList()
+            }
+        } else {
+            node.explore(pathSoFar.plus(id), doubleVisitsRemaining)
+        }
+    }
+}
+
+private fun List<String>.parseNodes(): Map<String, Node> {
+    val nodes = mutableMapOf<String, Node>()
+    forEach { line ->
+        val (a, b) = line.split("-")
+        val nodeA = nodes.getOrPut(a) { Node(a) }
+        val nodeB = nodes.getOrPut(b) { Node(b) }
+
+        nodeA.neighbours.putIfAbsent(b, nodeB)
+        nodeB.neighbours.putIfAbsent(a, nodeA)
+    }
+    return nodes
+}
+
+fun day12a(input: List<String>): Int {
+    val nodes = input.parseNodes()
+
+    return nodes["start"]!!
+        .explore(listOf("start"))
+        .count()
+}
+
 fun day12b(input: List<String>): Int {
-    return 0
+    val nodes = input.parseNodes()
+
+    return nodes["start"]!!
+        .explore(listOf("start"), 1)
+        .count()
 }
--- src/test/kotlin/v2021/day12.kt
+++ src/test/kotlin/v2021/day12.kt
@@ -7,28 +7,122 @@ import util.readInputLines
 
 class Day12aTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day12a(listOf()))
+        assertEquals(10, day12a(listOf(
+            "start-A",
+            "start-b",
+            "A-c",
+            "A-b",
+            "b-d",
+            "A-end",
+            "b-end"
+        )))
     }
 
     @Test
-    @Ignore
+    fun testExampleInput2() {
+        assertEquals(19, day12a(listOf(
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc"
+        )))
+    }
+
+    @Test
+    fun testExampleInput3() {
+        assertEquals(226, day12a(listOf(
+            "fs-end",
+            "he-DX",
+            "fs-he",
+            "start-DX",
+            "pj-DX",
+            "end-zg",
+            "zg-sl",
+            "zg-pj",
+            "pj-he",
+            "RW-he",
+            "fs-DX",
+            "pj-RW",
+            "zg-RW",
+            "start-pj",
+            "he-WI",
+            "zg-he",
+            "pj-fs",
+            "start-RW"
+        )))
+    }
+
+    @Test
     fun testActualInput() {
-        assertEquals(0, day12a(readInputLines("day12", 2021)))
+        assertEquals(4659, day12a(readInputLines("day12", 2021)))
     }
 }
 
 class Day12bTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day12b(listOf()))
+        assertEquals(36, day12b(listOf(
+            "start-A",
+            "start-b",
+            "A-c",
+            "A-b",
+            "b-d",
+            "A-end",
+            "b-end"
+        )))
+    }
+
+
+    @Test
+    fun testExampleInput2() {
+        assertEquals(103, day12b(listOf(
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc"
+        )))
     }
 
     @Test
-    @Ignore
+    fun testExampleInput3() {
+        assertEquals(3509, day12b(listOf(
+            "fs-end",
+            "he-DX",
+            "fs-he",
+            "start-DX",
+            "pj-DX",
+            "end-zg",
+            "zg-sl",
+            "zg-pj",
+            "pj-he",
+            "RW-he",
+            "fs-DX",
+            "pj-RW",
+            "zg-RW",
+            "start-pj",
+            "he-WI",
+            "zg-he",
+            "pj-fs",
+            "start-RW"
+        )))
+    }
+
+
+    @Test
     fun testActualInput() {
-        assertEquals(0, day12b(readInputLines("day12", 2021)))
+        assertEquals(148962, day12b(readInputLines("day12", 2021)))
     }
 }
