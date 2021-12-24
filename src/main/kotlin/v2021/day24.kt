2baddf3c18662da8d5d121f0f6db3688d91366f2
Jiri Bakker <jiribakker@bol.com>
2021-12-24 22:29:10 +0100

    Added explanation for day 24

--- src/main/kotlin/v2021/day24.kt
+++ src/main/kotlin/v2021/day24.kt
@@ -1,6 +1,48 @@
 package v2021
 
-// Based on MONAD logic
+/*
+    After analysing the input the MONAD behaviour can be rewritten as follows:
+
+    w          = model nr input digit
+    zRequired  = z value of next digit
+    zIn        = z value to use as input which will result in zRequired as output
+    restOffset = variable read from input instructions (each 6th line, 3rd element)
+    divisor    = 26 if restOffset < 0, 1 if restOffset > 0
+    divOffset  = variable read from input instructionns (each 16th line, 3rd element)
+
+    x1 = (zIn % 26) + restOffset
+    x2 = x1 != w ? 1 : 0
+
+    zRequired = ((zIn / divisor) * ((25 * x2) + 1)) + ((w + divOffset) * x2)
+
+    If restOffset < 0 it is possible to have x1 == w, which results in x2 being 0.
+    The resulting formula to find zIn can then be deduced as follows (note divisor will be 26):
+
+    w = x1
+    w = (zIn % 26) + restOffset
+    w - restOffset = zIn % 26
+    zRest = w - restOffset
+
+    Where zRest is the remainder of zIn % 26 to ensure w = x1. Then we use zRest below as follows:
+
+    zRequired = ((zIn / 26) * ((25 * 0) + 1)) + ((w + divOffset) * 0)
+    zRequired = (zIn / 26) * 1
+    zIn = zRequired * 26 + zRest
+
+    In case resultOffset > 0, it is not possible to have x1 == w (x1 will always be below 0, while
+    w is in 1-9). Therefore, the formula will be rewritten as follows (note divisor will be 1):
+
+    x2 = 1
+
+    zRequired = ((zIn / 1) * ((25 * 1) + 1) + ((w + divOffset) * 1)
+    zRequired = (zIn * 26) + (w + divOffset)
+    zRequired - w - divOffset = zIn * 26
+    zIn = (zRequired - w - divOffset) / 26
+
+    Note that we also need to verify that (zRequired - w - divOffset) is divisible by 26, because
+    the truncating of dividing by 26 could otherwise give us multiple possible values, while only
+    values exactly divisible by 26 are valid here.
+ */
 private fun List<String>.toComputeFunctions() =
     (5 .. 239 step 18)
         .map { i ->
