a88972aaded44d103f932d798ba20e7ee77ec518
Jiri Bakker <jiribakker@bol.com>
2021-12-24 16:40:05 +0100

    Implemented day 24

--- src/test/kotlin/v2021/day24.kt
+++ src/test/kotlin/v2021/day24.kt
@@ -1,34 +1,20 @@
 package v2021
 
 import org.junit.Assert.assertEquals
-import org.junit.Ignore
 import org.junit.Test
 import util.readInputLines
 
 class Day24aTests {
     @Test
-    @Ignore
-    fun testExampleInput1() {
-        assertEquals(0, day24a(listOf()))
-    }
-
-    @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day24a(readInputLines("day24", 2021)))
+        assertEquals(92928914999991L, day24a(readInputLines("day24", 2021)))
     }
 }
 
 class Day24bTests {
-    @Test
-    @Ignore
-    fun testExampleInput1() {
-        assertEquals(0, day24b(listOf()))
-    }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day24b(readInputLines("day24", 2021)))
+        assertEquals(91811211611981L, day24b(readInputLines("day24", 2021)))
     }
 }
