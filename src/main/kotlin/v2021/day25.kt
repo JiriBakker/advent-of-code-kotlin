c5909ff0c328f759d18d53677eb81730a20d21e3
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2021-12-25 07:32:05 +0100

    Implemented Day 25

--- src/main/kotlin/v2021/day25.kt
+++ src/main/kotlin/v2021/day25.kt
@@ -1,9 +1,67 @@
 package v2021
 
-import util.DoNotAutoExecute
+private typealias CucumberGrid = Map<Int, Map<Int, Char>>
 
-@DoNotAutoExecute
+private fun List<String>.parseCucumberGrid() =
+    withIndex().associate { (y, line) ->
+        y to line.withIndex().mapNotNull { (x, char) ->
+            if (char != '.') x to char
+            else null
+        }.toMap()
+    }
+
 fun day25a(input: List<String>): Int {
-    return 0
+    val height = input.size
+    val width  = input[0].length
+
+    var grid = input.parseCucumberGrid()
+
+    var steps = 0
+    do {
+        val nextGrid = mutableMapOf<Int, MutableMap<Int, Char>>()
+
+        var hasMoved = false
+
+        fun CucumberGrid.isLocationEmpty(x: Int, y: Int) =
+            get(y)?.containsKey(x) != true
+
+        fun canEastMoveInto(x: Int, y: Int) =
+            grid.isLocationEmpty(x, y)
+
+        fun canSouthMoveInto(x: Int, y: Int) =
+            grid[y]?.get(x) != 'v' && nextGrid.isLocationEmpty(x, y)
+
+        fun applyMoves(
+            cucumber: Char,
+            isMovePossible: (Int, Int) -> Boolean,
+            getNextX: (Int) -> Int = { it },
+            getNextY: (Int) -> Int = { it },
+        ) {
+            grid.forEach { (y, row) ->
+                row.forEach { (x, char) ->
+                    if (char == cucumber) {
+                        val xNext = getNextX(x)
+                        val yNext = getNextY(y)
+
+                        if (isMovePossible(xNext, yNext)) {
+                            nextGrid.getOrPut(yNext) { mutableMapOf() }[xNext] = cucumber
+                            hasMoved = true
+                        } else {
+                            nextGrid.getOrPut(y) { mutableMapOf() }[x] = cucumber
+                        }
+                    }
+                }
+            }
+        }
+
+        applyMoves('>', ::canEastMoveInto, getNextX = { x -> (x + 1) % width })
+        applyMoves('v', ::canSouthMoveInto, getNextY = { y -> (y + 1) % height })
+
+        grid = nextGrid
+
+        steps++
+    } while (hasMoved)
+
+    return steps
 }
 
--- src/test/kotlin/v2021/day25.kt
+++ src/test/kotlin/v2021/day25.kt
@@ -1,20 +1,27 @@
 package v2021
 
 import org.junit.Assert.assertEquals
-import org.junit.Ignore
 import org.junit.Test
 import util.readInputLines
 
 class Day25aTests {
     @Test
-    @Ignore
     fun testExampleInput1() {
-        assertEquals(0, day25a(listOf()))
+        assertEquals(58, day25a(listOf(
+            "v...>>.vv>",
+            ".vv>>.vv..",
+            ">>.>v>...v",
+            ">>v>>.>.v.",
+            "v>v.vv.v..",
+            ">.>>..v...",
+            ".vv..>.>v.",
+            "v.v..>>v.v",
+            "....v..v.>"
+        )))
     }
 
     @Test
-    @Ignore
     fun testActualInput() {
-        assertEquals(0, day25a(readInputLines("day25", 2021)))
+        assertEquals(582, day25a(readInputLines("day25", 2021)))
     }
 }
