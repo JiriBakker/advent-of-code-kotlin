45e975d5f7b23716115607cfa6051ba2c5faa66b
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2022-12-02 07:53:54 +0100

    Implemented Day 02 (2022)

---
+++ src/test/kotlin/v2022/day02.kt
@@ -0,0 +1,35 @@
+package v2022
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import util.readInputLines
+
+class Day02aTests {
+    @Test
+    fun testExampleInput1() {
+        assertEquals(15, day02a(listOf(
+            "A Y",
+            "B X",
+            "C Z")))
+    }
+
+    @Test
+    fun testActualInput() {
+        assertEquals(10718, day02a(readInputLines("day02", 2022)))
+    }
+}
+
+class Day02bTests {
+    @Test
+    fun testExampleInput1() {
+        assertEquals(12, day02b(listOf(
+            "A Y",
+            "B X",
+            "C Z")))
+    }
+
+    @Test
+    fun testActualInput() {
+        assertEquals(14652, day02b(readInputLines("day02", 2022)))
+    }
+}
