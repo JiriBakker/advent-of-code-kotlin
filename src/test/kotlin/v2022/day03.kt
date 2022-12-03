90e65814aea7bea48fb981997d0fca0fa3248191
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2022-12-03 08:05:01 +0100

    Implemented Day 03 (2022)

---
+++ src/test/kotlin/v2022/day03.kt
@@ -0,0 +1,41 @@
+package v2022
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import util.readInputLines
+
+class Day03aTests {
+    @Test
+    fun testExampleInput1() {
+        assertEquals(157, day03a(listOf(
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw")))
+    }
+
+    @Test
+    fun testActualInput() {
+        assertEquals(7766, day03a(readInputLines("day03", 2022)))
+    }
+}
+
+class Day03bTests {
+    @Test
+    fun testExampleInput1() {
+        assertEquals(70, day03b(listOf(
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw")))
+    }
+
+    @Test
+    fun testActualInput() {
+        assertEquals(2415, day03b(readInputLines("day03", 2022)))
+    }
+}
