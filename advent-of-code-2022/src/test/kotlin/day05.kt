8edbdbb6ad724f869e2727dcd21e0d26cf621976
Jiri Bakker <contact@jiri.dev>
2022-12-04 18:56:27 +0100

    New way of providing test input

--- advent-of-code-2022/src/test/kotlin/day05.kt
+++ advent-of-code-2022/src/test/kotlin/day05.kt
@@ -6,13 +6,15 @@ import util.trimInput
 class Day05aTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day05a(listOf(
-            "2-4,6-8",
-            "2-3,4-5",
-            "5-7,7-9",
-            "2-8,3-7",
-            "6-6,4-6",
-            "2-6,4-8")))
+        assertEquals(0, day05a(
+            """
+                2-4,6-8
+                2-3,4-5
+                5-7,7-9
+                2-8,3-7
+                6-6,4-6
+                2-6,4-8
+            """.trimInput()))
     }
 
     @Test
