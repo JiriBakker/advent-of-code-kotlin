07d181c678079c8b31bc8ae657c58931a64d5e65
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2022-12-10 08:41:59 +0100

    Implemented Day 10 (2022)

---
+++ advent-of-code-2022/src/main/kotlin/day10.kt
@@ -0,0 +1,69 @@
+import Day10.followInstructions
+
+fun day10a(input: List<String>): Long {
+    var sum = 0L
+
+    input.followInstructions { cycle, x ->
+        if ((cycle - 20) % 40 == 0) {
+            sum += cycle * x
+        }
+    }
+
+    return sum
+}
+
+fun day10b(input: List<String>): String {
+
+    val screen = (0 until 6).associateWith { mutableSetOf<Int>() }
+    fun screenToString() =
+        (0 until 6).joinToString("\n") { y ->
+            (0 until 40).joinToString("") { x ->
+                if (screen[y]!!.contains(x)) "#"
+                else "."
+            }
+        }
+
+    var spritePos = 1
+    fun draw(cycle: Int) {
+        val row = (cycle - 1) / 40
+        val col = (cycle - 1) % 40
+        if (col - spritePos in 0 .. 2) {
+            screen[row]!!.add(col)
+        }
+    }
+
+    var prevX = 0
+
+    input.followInstructions(
+        midCycleCallback =  { cycle, x ->
+            if (x != prevX) {
+                spritePos = x - 1
+                prevX = x
+            }
+            draw(cycle)
+        }
+    )
+
+    return screenToString()
+}
+
+object Day10 {
+    fun List<String>.followInstructions(midCycleCallback: (Int, Int) -> Unit) {
+        var x = 1
+        var cycle = 0
+
+        this.map { instruction ->
+            if (instruction.startsWith("addx ")) {
+                cycle++
+                midCycleCallback(cycle, x)
+                cycle++
+                midCycleCallback(cycle, x)
+                val amount = instruction.drop(5).toInt()
+                x += amount
+            } else {
+                cycle++
+                midCycleCallback(cycle, x)
+            }
+        }
+    }
+}
