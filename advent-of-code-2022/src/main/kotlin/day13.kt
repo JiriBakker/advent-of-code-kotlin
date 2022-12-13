2ee1136c0e9b7230ed394925ce1c88dce7a8713f
Jiri Bakker <contact@jiri.dev>
2022-12-13 18:31:23 +0100

    Implemented Day 13 (2022)

--- advent-of-code-2022/src/main/kotlin/day13.kt
+++ advent-of-code-2022/src/main/kotlin/day13.kt
@@ -1,7 +1,90 @@
-fun day13a(input: List<String>): Long {
-    return 0L
+import PacketInput.Companion.parsePacket
+
+fun day13a(input: List<String>) =
+    input
+        .chunked(3)
+        .map { (line1, line2, _) ->
+            val packet1 = parsePacket(line1)
+            val packet2 = parsePacket(line2)
+            packet1.compareTo(packet2)
+        }
+        .mapIndexed { index, result ->
+            if (result < 0) index + 1
+            else 0
+        }
+        .sum()
+
+fun day13b(input: List<String>): Int {
+    val packets =
+        input
+            .filter { it.isNotEmpty() }
+            .plus(listOf("[[2]]", "[[6]]"))
+            .map { parsePacket(it) }
+
+    val sortedPackets = packets.sortedWith(PacketInput::compareTo)
+
+    val indexOf2Divider = sortedPackets.indexOfFirst { it.toString() == "[[2]]" } + 1
+    val indexOf6Divider = sortedPackets.indexOfFirst { it.toString() == "[[6]]" } + 1
+
+    return indexOf2Divider * indexOf6Divider
 }
 
-fun day13b(input: List<String>): Long {
-    return 0L
+sealed class PacketInput {
+
+    abstract fun compareTo(other: PacketInput): Int
+
+    override fun toString(): String {
+        return when (this) {
+            is SingleInteger -> this.nr.toString()
+            is PacketInputList -> "[${this.children.joinToString(",")}]"
+        }
+    }
+
+    companion object {
+        fun parsePacket(input: String): PacketInput {
+            fun parse(input: String): Pair<PacketInput, Int> {
+                val children = mutableListOf<PacketInput>()
+                var index = 0
+                while (index in input.indices) {
+                    if (input[index].isDigit()) {
+                        val nr = input.drop(index).takeWhile { it.isDigit() }
+                        children.add(SingleInteger(nr.toInt()))
+                        index += nr.length
+                    } else if (input[index] == '[') {
+                        val (nestedChildren, lengthParsed) = parse(input.drop(index + 1))
+                        children.add(nestedChildren)
+                        index += lengthParsed + 2
+                    } else if (input[index] == ']') {
+                        break
+                    } else {
+                        index++
+                    }
+                }
+                return PacketInputList(children) to index
+            }
+            return parse(input.drop(1).dropLast(1)).first
+        }
+    }
+}
+
+class PacketInputList(val children: List<PacketInput>): PacketInput() {
+     override operator fun compareTo(other: PacketInput): Int {
+        return when (other) {
+            is SingleInteger -> this.compareTo(PacketInputList(listOf(other)))
+            is PacketInputList ->
+                this.children.zip(other.children)
+                    .map { it.first.compareTo(it.second) }
+                    .firstOrNull { it != 0 }
+                        ?: this.children.size.compareTo(other.children.size)
+        }
+    }
+}
+
+class SingleInteger(val nr: Int) : PacketInput() {
+    override operator fun compareTo(other: PacketInput): Int {
+        return when (other) {
+            is SingleInteger -> this.nr.compareTo(other.nr)
+            is PacketInputList -> PacketInputList(listOf(this)).compareTo(other)
+        }
+    }
 }
--- advent-of-code-2022/src/test/kotlin/day13.kt
+++ advent-of-code-2022/src/test/kotlin/day13.kt
@@ -5,25 +5,69 @@ import util.readInputLines
 class Day13aTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day13a(listOf(
-            "")))
+        assertEquals(13, day13a(listOf(
+            "[1,1,3,1,1]",
+            "[1,1,5,1,1]",
+            "",
+            "[[1],[2,3,4]]",
+            "[[1],4]",
+            "",
+            "[9]",
+            "[[8,7,6]]",
+            "",
+            "[[4,4],4,4]",
+            "[[4,4],4,4,4]",
+            "",
+            "[7,7,7,7]",
+            "[7,7,7]",
+            "",
+            "[]",
+            "[3]",
+            "",
+            "[[[]]]",
+            "[[]]",
+            "",
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day13a(readInputLines("day13")))
+        assertEquals(6395, day13a(readInputLines("day13")))
     }
 }
 
 class Day13bTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day13b(listOf(
-            "")))
+        assertEquals(140, day13b(listOf(
+            "[1,1,3,1,1]",
+            "[1,1,5,1,1]",
+            "",
+            "[[1],[2,3,4]]",
+            "[[1],4]",
+            "",
+            "[9]",
+            "[[8,7,6]]",
+            "",
+            "[[4,4],4,4]",
+            "[[4,4],4,4,4]",
+            "",
+            "[7,7,7,7]",
+            "[7,7,7]",
+            "",
+            "[]",
+            "[3]",
+            "",
+            "[[[]]]",
+            "[[]]",
+            "",
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day13b(readInputLines("day13")))
+        assertEquals(24921, day13b(readInputLines("day13")))
     }
 }
