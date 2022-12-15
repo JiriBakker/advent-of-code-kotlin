f8b881d266737de7c0c4472f64e7f65fcff1d8ec
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2022-12-15 09:13:27 +0100

    Implemented Day 15 (2022)

--- advent-of-code-2022/src/main/kotlin/day15.kt
+++ advent-of-code-2022/src/main/kotlin/day15.kt
@@ -1,7 +1,88 @@
-fun day15a(input: List<String>): Long {
-    return 0L
+import Day15.parseSensors
+import java.math.BigInteger
+import kotlin.math.abs
+
+fun day15a(input: List<String>, y: Long = 10): Int {
+    val sensors = input.parseSensors()
+    val beacons = sensors.map { it.beaconX to it.beaconY }.toSet()
+
+    val minX = sensors.minOf { it.minX }
+    val maxX = sensors.maxOf { it.maxX }
+
+    val withinSensorRangeCount =
+        (minX - 1 .. maxX + 1).count { x ->
+            sensors.any { it.isInRange(x, y) }
+        }
+
+    val beaconCount =
+        beacons.count { it.second == y }
+
+    return withinSensorRangeCount - beaconCount
 }
 
-fun day15b(input: List<String>): Long {
-    return 0L
+fun day15b(input: List<String>, max: Long): BigInteger {
+    val sensors = input.parseSensors()
+
+    sensors.forEach { sensor ->
+        sensor.edgeCoordinates
+            .filter { (x, y) -> x in 0..max && y in 0..max }
+            .forEach { (x, y) ->
+                if (sensors.none { it.isInRange(x, y) }) {
+                    return 4000000.toBigInteger() * x.toBigInteger() + y.toBigInteger()
+                }
+            }
+    }
+
+    throw Exception("No possible beacon location found")
+}
+
+object Day15 {
+
+    fun List<String>.parseSensors() =
+        this.map { line ->
+            val lineParts = line.split(" ")
+            val x = lineParts[2].drop(2).dropLast(1).toLong()
+            val y = lineParts[3].drop(2).dropLast(1).toLong()
+            val beaconX = lineParts[8].drop(2).dropLast(1).toLong()
+            val beaconY = lineParts[9].drop(2).toLong()
+            Sensor(x, y, beaconX, beaconY)
+        }
+
+    class Sensor(val x: Long, val y: Long, val beaconX: Long, val beaconY: Long) {
+        private val beaconFreeRange = abs(x - beaconX) + abs(y - beaconY)
+
+        private val minY = y - beaconFreeRange
+        private val maxY = y + beaconFreeRange
+
+        val minX = x - beaconFreeRange
+        val maxX = x + beaconFreeRange
+
+        fun isInRange(otherX: Long, otherY: Long) =
+            (abs(x - otherX) + abs(y - otherY)) <= beaconFreeRange
+
+        val edgeCoordinates get() =
+            sequence {
+                var curX = minX - 1
+                (y downTo minY).forEach {
+                    yield(curX to it)
+                    curX++
+                }
+
+                (minY..y).forEach {
+                    yield(curX to it)
+                    curX++
+                }
+
+                (y..maxY).forEach {
+                    yield(curX to it)
+                    curX--
+                }
+
+                (maxY downTo y).forEach {
+                    yield(curX to it)
+                    curX--
+                }
+            }
+
+    }
 }
--- advent-of-code-2022/src/test/kotlin/day15.kt
+++ advent-of-code-2022/src/test/kotlin/day15.kt
@@ -1,29 +1,57 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import util.readInputLines
+import java.math.BigInteger
 
 class Day15aTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day15a(listOf(
-            "")))
+        assertEquals(26, day15a(listOf(
+            "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+            "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+            "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+            "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+            "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+            "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+            "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+            "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+            "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+            "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+            "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+            "Sensor at x=20, y=1: closest beacon is at x=15, y=3")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day15a(readInputLines("day15")))
+        assertEquals(5335787, day15a(readInputLines("day15"), y = 2000000))
     }
 }
 
 class Day15bTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day15b(listOf(
-            "")))
+        assertEquals(56000011.toBigInteger(), day15b(listOf(
+            "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+            "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+            "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+            "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+            "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+            "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+            "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+            "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+            "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+            "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+            "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+            "Sensor at x=20, y=1: closest beacon is at x=15, y=3"),
+            max = 20))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day15b(readInputLines("day15")))
+        assertEquals(BigInteger.valueOf(13673971349056), day15b(readInputLines("day15"), max = 4000000))
     }
 }
