24b08ee75ddea257eb537be9d91597c8e878fcd0
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2022-12-17 21:12:21 +0100

    Implemented Day 17 (2022) It ain't pretty (yet)

--- advent-of-code-2022/src/test/kotlin/day17.kt
+++ advent-of-code-2022/src/test/kotlin/day17.kt
@@ -5,25 +5,25 @@ import util.readInputLines
 class Day17aTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day17a(listOf(
-            "")))
+        assertEquals(3068, day17a(listOf(
+            ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day17a(readInputLines("day17")))
+        assertEquals(3200, day17a(readInputLines("day17")))
     }
 }
 
 class Day17bTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day17b(listOf(
-            "")))
+        assertEquals(1514285714288L, day17b(listOf(
+            ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day17b(readInputLines("day17")))
+        assertEquals(1584927536247L, day17b(readInputLines("day17")))
     }
 }
