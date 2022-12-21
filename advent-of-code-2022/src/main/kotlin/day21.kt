cc154c44d40248c41a34a239f641e81fea9739e6
Jiri Bakker <contact@jiri.dev>
2022-12-21 20:06:00 +0100

    Implemented Day 21 (2022)

--- advent-of-code-2022/src/main/kotlin/day21.kt
+++ advent-of-code-2022/src/main/kotlin/day21.kt
@@ -1,7 +1,136 @@
+import Day21.parseMonkeys
+import Day21.ComputedShout
+import Day21.NumberShout
+
 fun day21a(input: List<String>): Long {
-    return 0L
+    val monkeys = input.parseMonkeys()
+
+    val root = monkeys["root"]!!
+
+    return root.shout().result
 }
 
 fun day21b(input: List<String>): Long {
-    return 0L
+    val monkeys =
+        input.parseMonkeys(
+            rootOperation = { a, b ->
+                if (a is ComputedShout && a.containsHuman()) {
+                    a.solveHuman(b.result)
+                } else if (b is ComputedShout && b.containsHuman()) {
+                    b.solveHuman(a.result)
+                } else if (a is NumberShout && a.containsHuman()) {
+                    b.result
+                } else if (b is NumberShout && b.containsHuman()) {
+                    a.result
+                } else {
+                    throw Exception("Inconsistent state, no human found")
+                }
+            }
+        )
+
+    val root = monkeys["root"]!!
+
+    return root.shout().result
+}
+
+object Day21 {
+
+    fun List<String>.parseMonkeys(
+        rootOperation: ((MonkeyShout, MonkeyShout) -> Long)? = null
+    ): Map<String, Monkey> {
+        val monkeys = mutableMapOf<String, Monkey>()
+        this.forEach { line ->
+            val parts = line.split(" ")
+            val name = parts[0].trimEnd(':')
+            val monkey =
+                if (parts.size == 4) {
+                    val operation =
+                        if (name == "root" && rootOperation != null) {
+                            rootOperation
+                        } else {
+                            when (parts[2]) {
+                                "*" -> { a, b -> a.result * b.result }
+                                "/" -> { a, b -> a.result / b.result }
+                                "+" -> { a, b -> a.result + b.result }
+                                else -> { a, b -> a.result - b.result }
+                            }
+                        }
+
+                    val monkey1 = monkeys[parts[1]]!!.shout()
+                    val monkey2 = monkeys[parts[3]]!!.shout()
+
+                    Monkey(name) {
+                        ComputedShout(
+                            operation.invoke(monkey1, monkey2),
+                            monkey1,
+                            monkey2,
+                            parts[2]
+                        )
+                    }
+                } else {
+                    Monkey(name) { NumberShout(parts[1].toLong(), isHuman = name == "humn") }
+                }
+
+            monkeys[name] = monkey
+        }
+        return monkeys
+    }
+
+    class Monkey(val name: String, val shout: () -> MonkeyShout)
+
+    sealed class MonkeyShout {
+        abstract val result: Long
+        abstract fun containsHuman(): Boolean
+    }
+
+    class NumberShout(
+        number: Long,
+        private val isHuman: Boolean = false
+    ) : MonkeyShout() {
+        override val result = number
+        override fun containsHuman() = isHuman
+        override fun toString() = result.toString()
+    }
+
+    class ComputedShout(
+        override val result: Long,
+        private val monkey1: MonkeyShout,
+        private val monkey2: MonkeyShout,
+        private val operator: String
+    ) : MonkeyShout() {
+        override fun toString() =
+            "($monkey1 $operator $monkey2)"
+
+        override fun containsHuman() =
+            monkey1.containsHuman() || monkey2.containsHuman()
+
+        fun solveHuman(otherResult: Long): Long {
+            return if (monkey1.containsHuman()) {
+                val newResult = when (operator) {
+                    "/" -> otherResult * monkey2.result
+                    "-" -> otherResult + monkey2.result
+                    "+" -> otherResult - monkey2.result
+                    "*" -> otherResult / monkey2.result
+                    else -> throw Exception("Unknown operator: $operator")
+                }
+                when (monkey1) {
+                    is NumberShout -> newResult
+                    is ComputedShout -> monkey1.solveHuman(newResult)
+                }
+            } else {
+                val newResult = when (operator) {
+                    "/" -> monkey1.result / otherResult
+                    "-" -> monkey1.result - otherResult
+                    "+" -> otherResult - monkey1.result
+                    "*" -> otherResult / monkey1.result
+                    else -> throw Exception("Unknown operator: $operator")
+                }
+
+                when (monkey2) {
+                    is NumberShout -> newResult
+                    is ComputedShout -> monkey2.solveHuman(newResult)
+                }
+            }
+        }
+    }
 }
--- advent-of-code-2022/src/test/kotlin/day21.kt
+++ advent-of-code-2022/src/test/kotlin/day21.kt
@@ -5,25 +5,53 @@ import util.readInputLines
 class Day21aTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day21a(listOf(
-            "")))
+        assertEquals(152, day21a(listOf(
+            "root: pppw + sjmn",
+            "dbpl: 5",
+            "cczh: sllz + lgvd",
+            "zczc: 2",
+            "ptdq: humn - dvpt",
+            "dvpt: 3",
+            "lfqf: 4",
+            "humn: 5",
+            "ljgn: 2",
+            "sjmn: drzm * dbpl",
+            "sllz: 4",
+            "pppw: cczh / lfqf",
+            "lgvd: ljgn * ptdq",
+            "drzm: hmdt - zczc",
+            "hmdt: 32")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day21a(readInputLines("day21")))
+        assertEquals(291425799367130L, day21a(readInputLines("day21")))
     }
 }
 
 class Day21bTests {
     @Test
     fun testExampleInput1() {
-        assertEquals(0, day21b(listOf(
-            "")))
+        assertEquals(301, day21b(listOf(
+            "root: pppw + sjmn",
+            "dbpl: 5",
+            "cczh: sllz + lgvd",
+            "zczc: 2",
+            "ptdq: humn - dvpt",
+            "dvpt: 3",
+            "lfqf: 4",
+            "humn: 5",
+            "ljgn: 2",
+            "sjmn: drzm * dbpl",
+            "sllz: 4",
+            "pppw: cczh / lfqf",
+            "lgvd: ljgn * ptdq",
+            "drzm: hmdt - zczc",
+            "hmdt: 32")))
     }
 
     @Test
     fun testActualInput() {
-        assertEquals(0, day21b(readInputLines("day21")))
+        assertEquals(3219579395609L, day21b(readInputLines("day21")))
     }
 }
