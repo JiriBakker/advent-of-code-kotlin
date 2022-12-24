aec05f8ab1b680f460c2674392a615ba00d25ac8
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2022-12-24 18:43:42 +0100

    Refactored Day 23 (2022)

--- advent-of-code-2022/src/main/kotlin/day23.kt
+++ advent-of-code-2022/src/main/kotlin/day23.kt
@@ -1,219 +1,74 @@
-import Day23.computePotential
-import Day23.moveElves
-import Day23.parseGrid
+import Day23.countEmptyTiles
+import Day23.parseElves
+import Day23.runRoundsWhile
 import util.max
 import util.min
 
 fun day23a(input: List<String>): Int {
-    val initialGrid = input.parseGrid()
-    var grid = initialGrid
+    val initialGrid = input.parseElves()
 
-    var directionRules =
-        listOf(
-            { x: Int, y: Int ->
-                val ne = grid[y - 1]?.get(x - 1)
-                val n = grid[y - 1]?.get(x)
-                val nw = grid[y - 1]?.get(x + 1)
-                if (ne == null && n == null && nw == null) {
-                    x to (y - 1)
-                } else {
-                    null
-                }
-            },
-            { x: Int, y: Int ->
-                val sw = grid[y + 1]?.get(x - 1)
-                val s = grid[y + 1]?.get(x)
-                val se = grid[y + 1]?.get(x + 1)
-                if (sw == null && s == null && se == null) {
-                    x to (y + 1)
-                } else {
-                    null
-                }
-            },
-            { x: Int, y: Int ->
-                val nw = grid[y - 1]?.get(x - 1)
-                val w = grid[y]?.get(x - 1)
-                val sw = grid[y + 1]?.get(x - 1)
-                if (nw == null && w == null && sw == null) {
-                    (x - 1) to y
-                } else {
-                    null
-                }
-            },
-            { x: Int, y: Int ->
-                val ne = grid[y - 1]?.get(x + 1)
-                val e = grid[y]?.get(x + 1)
-                val se = grid[y + 1]?.get(x + 1)
-                if (ne == null && e == null && se == null) {
-                    (x + 1) to y
-                } else {
-                    null
-                }
-            }
-        )
-
-    repeat(10) {
-        //println("Round $it")
-        val potentialGrid = grid.computePotential(directionRules)
-
-        grid = grid.moveElves(directionRules, potentialGrid)
-
-        directionRules = directionRules.drop(1).plus(directionRules.first())
-    }
-
-    var minX = Int.MAX_VALUE
-    var maxX = Int.MIN_VALUE
-    var minY = Int.MAX_VALUE
-    var maxY = Int.MIN_VALUE
-
-    grid.forEach { (y, row) ->
-        minY = min(minY, y)
-        maxY = max(maxY, y)
-        row.forEach { (x, _) ->
-            minX = min(minX, x)
-            maxX = max(maxX, x)
+    val (finalGrid, _) =
+        initialGrid.runRoundsWhile { _, roundNr ->
+            roundNr < 10
         }
-    }
 
-
-    var emptyTiles = 0
-    (minY .. maxY).forEach { y ->
-        (minX..maxX).forEach { x ->
-            if (grid[y]?.get(x) == null) {
-                emptyTiles++
-            }
-        }
-    }
-
-    return emptyTiles
+    return finalGrid.countEmptyTiles()
 }
 
 fun day23b(input: List<String>): Int {
-    val initialGrid = input.parseGrid()
-    var grid = initialGrid
+    val initialElves = input.parseElves()
 
-    var directionRules =
-        listOf(
-            { x: Int, y: Int ->
-                val ne = grid[y - 1]?.get(x - 1)
-                val n = grid[y - 1]?.get(x)
-                val nw = grid[y - 1]?.get(x + 1)
-                if (ne == null && n == null && nw == null) {
-                    x to (y - 1)
-                } else {
-                    null
-                }
-            },
-            { x: Int, y: Int ->
-                val sw = grid[y + 1]?.get(x - 1)
-                val s = grid[y + 1]?.get(x)
-                val se = grid[y + 1]?.get(x + 1)
-                if (sw == null && s == null && se == null) {
-                    x to (y + 1)
-                } else {
-                    null
-                }
-            },
-            { x: Int, y: Int ->
-                val nw = grid[y - 1]?.get(x - 1)
-                val w = grid[y]?.get(x - 1)
-                val sw = grid[y + 1]?.get(x - 1)
-                if (nw == null && w == null && sw == null) {
-                    (x - 1) to y
-                } else {
-                    null
-                }
-            },
-            { x: Int, y: Int ->
-                val ne = grid[y - 1]?.get(x + 1)
-                val e = grid[y]?.get(x + 1)
-                val se = grid[y + 1]?.get(x + 1)
-                if (ne == null && e == null && se == null) {
-                    (x + 1) to y
-                } else {
-                    null
-                }
-            }
-        )
+    var prevElves = initialElves
 
-    var round = 1
-    while (true) {
-        //println("Round $it")
-        val prevGrid = grid
-        val potentialGrid = grid.computePotential(directionRules)
-
-        grid = grid.moveElves(directionRules, potentialGrid)
-
-        directionRules = directionRules.drop(1).plus(directionRules.first())
-
-        val hasChanged =
-            prevGrid.any { (y, prevRow) ->
-                val row = grid[y]
-                row == null || prevRow.any { (x, elf) ->
-                    row[x] == null
-                }
-            }
-
-        if (!hasChanged) {
-            return round
+    val (_, finalRoundNr) =
+        initialElves.runRoundsWhile { elves, _ ->
+            (elves != prevElves)
+                .also { prevElves = elves }
         }
-        round++
-    }
+
+
+    return finalRoundNr
 }
 
 object Day23 {
 
-    fun Map<Int, Map<Int, Int>>.computePotential(
-        directionRules: List<(Int, Int) -> Pair<Int, Int>?>
-    ): Map<Int, Map<Int, Int>> {
-        val potentialGrid = mutableMapOf<Int, MutableMap<Int, Int>>()
+    fun Map<Int, Map<Int, Int>>.runRoundsWhile(
+        runWhilePredicate: (Map<Int, Map<Int, Int>>, Int) -> Boolean
+    ): Pair<Map<Int, Map<Int, Int>>, Int> {
+        var elves = this
+        var moveRules = initialMoveRules
 
-        fun setPotential(x: Int, y: Int) {
-            val potentialRow = potentialGrid.getOrPut(y) { mutableMapOf() }
-            potentialRow[x] = potentialRow.getOrDefault(x, 0) + 1
-        }
+        var roundNr = 0
+        do {
+            val collisionTiles =
+                elves.move(moveRules)
+                    .filterCollisionPositions()
 
-        fun hasNeighbours(x: Int, y: Int) =
-            this[y - 1]?.get(x) != null || // N
-                this[y - 1]?.get(x + 1) != null || // NE
-                this[y]?.get(x + 1) != null || // E
-                this[y + 1]?.get(x + 1) != null || // SE
-                this[y + 1]?.get(x) != null || // S
-                this[y + 1]?.get(x - 1) != null || // SW
-                this[y]?.get(x - 1) != null || // W
-                this[y - 1]?.get(x - 1) != null // NW
+            elves = elves.move(moveRules, collisionTiles)
 
+            moveRules = moveRules.drop(1).plus(moveRules.first())
 
-        this.forEach { (y, row) ->
-            row.forEach { (x, elf) ->
-                if (hasNeighbours(x, y)) {
-                    val potentialNextPos =
-                        directionRules.firstNotNullOfOrNull { rule ->
-                            rule.invoke(x, y)
-                        }
+            roundNr++
+        } while (runWhilePredicate.invoke(elves, roundNr))
 
-                    if (potentialNextPos != null) {
-                        setPotential(potentialNextPos.first, potentialNextPos.second)
-                        return@forEach
-                    }
-                }
-
-//                setPotential(x, y)
-            }
-        }
-
-        return potentialGrid
+        return elves to roundNr
     }
 
-    fun Map<Int, Map<Int, Int>>.moveElves(
-        directionRules: List<(Int, Int) -> Pair<Int, Int>?>,
-        potentialGrid: Map<Int, Map<Int, Int>>
-    ): Map<Int, Map<Int, Int>> {
-        val nextGrid = mutableMapOf<Int, MutableMap<Int, Int>>()
+    private fun Map<Int, Map<Int, Int>>.filterCollisionPositions() =
+        this.flatMap { (y, row) ->
+                row.filter { it.value > 1 }
+                    .map { (x, _) -> x to y }
+            }
+            .toSet()
 
-        fun set(x: Int, y: Int) {
-            //println("Setting $x $y")
-            val nextRow = nextGrid.getOrPut(y) { mutableMapOf() }
+    private fun Map<Int, Map<Int, Int>>.move(
+        moveRules: List<(Map<Int, Map<Int, Int>>, Int, Int) -> Pair<Int, Int>?>,
+        collisionTiles: Set<Pair<Int, Int>> = emptySet()
+    ): Map<Int, Map<Int, Int>> {
+        val nextElves = mutableMapOf<Int, MutableMap<Int, Int>>()
+
+        fun setElf(x: Int, y: Int) {
+            val nextRow = nextElves.getOrPut(y) { mutableMapOf() }
             nextRow[x] = nextRow.getOrDefault(x, 0) + 1
         }
 
@@ -229,40 +84,118 @@ object Day23 {
 
 
         this.forEach { (y, row) ->
-            row.forEach { (x, elf) ->
-                //print("Checking $x $y")
+            row.forEach { (x, _) ->
                 if (hasNeighbours(x, y)) {
                     val potentialNextPos =
-                        directionRules.firstNotNullOfOrNull { rule ->
-                            rule.invoke(x, y)
+                        moveRules.firstNotNullOfOrNull { rule ->
+                            rule.invoke(this, x, y)
                         }
 
                     if (potentialNextPos != null) {
-                        val collidingElf = potentialGrid[potentialNextPos.second]?.get(potentialNextPos.first)
-                        if (collidingElf == null || collidingElf <= 1) {
-                            set(potentialNextPos.first, potentialNextPos.second)
+                        val isCollisionTile = collisionTiles.contains(potentialNextPos)
+                        if (!isCollisionTile) {
+                            setElf(potentialNextPos.first, potentialNextPos.second)
                             return@forEach
                         }
                     }
                 }
 
-                set(x, y)
+                setElf(x, y)
             }
         }
 
-        return nextGrid
+        return nextElves
     }
 
-    fun List<String>.parseGrid(): Map<Int, Map<Int, Int>> {
-        val grid = mutableMapOf<Int, MutableMap<Int, Int>>()
+    fun Map<Int, Map<Int, Int>>.countEmptyTiles(): Int {
+        var minX = Int.MAX_VALUE
+        var maxX = Int.MIN_VALUE
+        var minY = Int.MAX_VALUE
+        var maxY = Int.MIN_VALUE
+
+        this.forEach { (y, row) ->
+            minY = min(minY, y)
+            maxY = max(maxY, y)
+            row.forEach { (x, _) ->
+                minX = min(minX, x)
+                maxX = max(maxX, x)
+            }
+        }
+
+        return (minY .. maxY).sumOf { y ->
+            (minX..maxX).count { x ->
+                this[y]?.get(x) == null
+            }
+        }
+    }
+
+    fun List<String>.parseElves(): Map<Int, Map<Int, Int>> {
+        val elves = mutableMapOf<Int, MutableMap<Int, Int>>()
         this.forEachIndexed { y, line ->
             line.forEachIndexed { x, char ->
                 if (char == '#') {
-                    val row = grid.getOrPut(y) { mutableMapOf() }
+                    val row = elves.getOrPut(y) { mutableMapOf() }
                     row[x] = 1
                 }
             }
         }
-        return grid
+        return elves
     }
+
+    private val moveNorthRule =
+        { elves: Map<Int, Map<Int, Int>>, x: Int, y: Int ->
+            val ne = elves[y - 1]?.get(x - 1)
+            val n = elves[y - 1]?.get(x)
+            val nw = elves[y - 1]?.get(x + 1)
+            if (ne == null && n == null && nw == null) {
+                x to (y - 1)
+            } else {
+                null
+            }
+        }
+
+    private val moveSouthRule =
+        { elves: Map<Int, Map<Int, Int>>, x: Int, y: Int ->
+            val sw = elves[y + 1]?.get(x - 1)
+            val s = elves[y + 1]?.get(x)
+            val se = elves[y + 1]?.get(x + 1)
+            if (sw == null && s == null && se == null) {
+                x to (y + 1)
+            } else {
+                null
+            }
+        }
+
+    private val moveWestRule =
+        { elves: Map<Int, Map<Int, Int>>, x: Int, y: Int ->
+            val nw = elves[y - 1]?.get(x - 1)
+            val w = elves[y]?.get(x - 1)
+            val sw = elves[y + 1]?.get(x - 1)
+            if (nw == null && w == null && sw == null) {
+                (x - 1) to y
+            } else {
+                null
+            }
+        }
+
+
+    private val moveEastRule =
+        { elves: Map<Int, Map<Int, Int>>, x: Int, y: Int ->
+            val ne = elves[y - 1]?.get(x + 1)
+            val e = elves[y]?.get(x + 1)
+            val se = elves[y + 1]?.get(x + 1)
+            if (ne == null && e == null && se == null) {
+                (x + 1) to y
+            } else {
+                null
+            }
+        }
+
+    private val initialMoveRules =
+        listOf(
+            moveNorthRule,
+            moveSouthRule,
+            moveWestRule,
+            moveEastRule
+        )
 }
