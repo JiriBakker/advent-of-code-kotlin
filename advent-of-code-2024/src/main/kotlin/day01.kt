81981a13e39b2430408f1ceba4c9555ad5222f9f
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2024-12-01 21:37:24 +0100

    Minor improvements

--- advent-of-code-2024/src/main/kotlin/day01.kt
+++ advent-of-code-2024/src/main/kotlin/day01.kt
@@ -4,18 +4,21 @@ fun day01a(input: List<String>): Int {
     val (list1, list2) = parseInput(input)
 
     return list1.sorted().zip(list2.sorted())
-        .sumOf { (a, b) -> abs(a - b) }
+        .sumDistances()
 }
 
 fun day01b(input: List<String>): Int {
     val (list1, list2) = parseInput(input)
 
-    val letterCounts = list2.groupBy { it }.mapValues { it.value.size }.toMap()
+    val nrCounts = list2.groupBy { it }.mapValues { it.value.size }.toMap()
 
-    return list1.sumOf { it * letterCounts.getOrDefault(it, 0) }
+    return list1.sumOf { it * nrCounts.getOrDefault(it, 0) }
 }
 
 private fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> =
     input
         .map { str -> str.split("   ").map(String::toInt).let { it[0] to it[1] } }
         .unzip()
+
+private fun List<Pair<Int,Int>>.sumDistances() =
+    sumOf { (a, b) -> abs(a - b) }
