20412463c03e80ff6c1d2e8409f2916eeb20e7e5
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2024-12-07 07:50:00 +0100

    Implemented Day 7

---
+++ advent-of-code-2024/src/main/kotlin/day07.kt
@@ -0,0 +1,57 @@
+import util.sumOfLong
+
+fun day07a(input: List<String>) =
+    input.findCalibrationResult(baseOperators)
+
+fun day07b(input: List<String>) =
+    input.findCalibrationResult(baseOperators + ::concatLong)
+
+
+private fun concatLong(a: Long, b: Long): Long {
+    return (a.toString() + b.toString()).toLong()
+}
+
+private val baseOperators = listOf<((Long, Long) -> Long)>(
+    Long::plus,
+    Long::times,
+)
+
+private fun List<String>.findCalibrationResult(operators: List<(Long, Long) -> Long>): Long {
+    return this.sumOfLong { line ->
+        val (testValue, numbersString) = line.split(": ")
+        val numbers = numbersString.split(" ").map { it.toLong() }
+
+        if (hasValidOperatorCombination(numbers, 1, numbers[0], testValue.toLong(), operators)) {
+            testValue.toLong()
+        } else {
+            0
+        }
+    }
+}
+
+private fun hasValidOperatorCombination(
+    numbers: List<Long>,
+    curIndex: Int,
+    valueSoFar: Long,
+    testValue: Long,
+    operators: List<((Long, Long) -> Long)>
+): Boolean {
+    if (curIndex == numbers.size) {
+        return valueSoFar == testValue
+    }
+
+    for (op in operators) {
+        if (hasValidOperatorCombination(
+                numbers,
+                curIndex + 1,
+                op(valueSoFar, numbers[curIndex]),
+                testValue,
+                operators
+        )) {
+            return true
+        }
+    }
+
+    return false
+}
+
