9c5a356b90605f5dbc64206f5e44d9ac19ec5d79
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2025-12-08 15:44:26 +0100

    Simplified day 08

--- advent-of-code-2025/src/main/kotlin/day08.kt
+++ advent-of-code-2025/src/main/kotlin/day08.kt
@@ -3,6 +3,7 @@ import kotlin.collections.component1
 import kotlin.collections.component2
 import kotlin.collections.component3
 import kotlin.collections.mapIndexed
+import kotlin.math.min
 import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlin.text.split
@@ -14,6 +15,7 @@ private data class JunctionBox(
     var circuit: Int
 ) {
     fun distanceTo(other: JunctionBox) =
+        // NOT Manhattan distance, but Euclidian distance
         sqrt(
             (x - other.x).pow(2)
             + (y - other.y).pow(2)
@@ -24,7 +26,7 @@ private data class JunctionBox(
 fun day08a(input: List<String>, nrOfConnections: Int): Int {
     val junctionBoxes = parseJunctionBoxes(input)
 
-    val (_, circuits) = connectJunctionBoxes(junctionBoxes).take(nrOfConnections).last()
+    val (_, circuits) = connectJunctionBoxes(junctionBoxes, nrOfConnections)
 
     return circuits.values.map { it.size }.sortedDescending().take(3).reduce(Int::times)
 }
@@ -32,12 +34,12 @@ fun day08a(input: List<String>, nrOfConnections: Int): Int {
 fun day08b(input: List<String>): Long {
     val junctionBoxes = parseJunctionBoxes(input)
 
-    val (lastConnection, _) = connectJunctionBoxes(junctionBoxes).filter { it.first != null }.last()
+    val (lastConnection, _) = connectJunctionBoxes(junctionBoxes)
 
-    return lastConnection!!.first.x.toLong() * lastConnection.second.x.toLong()
+    return lastConnection.first.x.toLong() * lastConnection.second.x.toLong()
 }
 
-private fun connectJunctionBoxes(junctionBoxes: List<JunctionBox>): Sequence<Pair<Pair<JunctionBox, JunctionBox>?, Map<Int, List<JunctionBox>>>> {
+private fun connectJunctionBoxes(junctionBoxes: List<JunctionBox>, nrOfConnections: Int = Int.MAX_VALUE): Pair<Pair<JunctionBox, JunctionBox>, Map<Int, List<JunctionBox>>> {
     val distances = mutableMapOf<Pair<JunctionBox, JunctionBox>, Double>()
     for (i in junctionBoxes.indices) {
         for (j in i + 1 until junctionBoxes.size) {
@@ -47,25 +49,25 @@ private fun connectJunctionBoxes(junctionBoxes: List<JunctionBox>): Sequence<Pai
     }
 
     val sortedDistances = distances.toList().sortedBy { (_, distance) -> distance }
-    val circuits = junctionBoxes.associate { it.circuit to mutableListOf(it) }.toMutableMap()
+    val circuits = junctionBoxes.associate { it.circuit to listOf(it) }.toMutableMap()
+    var lastConnection: Pair<JunctionBox, JunctionBox>? = null
 
-    return sequence {
-        sortedDistances.forEach { (junctionBoxPair, _) ->
-            val (junctionBox1, junctionBox2) = junctionBoxPair
-            val circuit1 = junctionBox1.circuit
-            val circuit2 = junctionBox2.circuit
+    for (i in 0 until min(nrOfConnections, sortedDistances.size)) {
+        val (connection, _) = sortedDistances[i]
+        val circuit1 = connection.first.circuit
+        val circuit2 = connection.second.circuit
 
-            if (circuit1 != circuit2) {
-                circuits[circuit1] = circuits[circuit1]!!.plus(circuits[circuit2]!!).toMutableList()
-                circuits[circuit1]!!.forEach { circuit -> circuit.circuit = circuit1 }
-                circuits.remove(circuit2)
+        if (circuit1 == circuit2) continue
 
-                yield(junctionBoxPair to circuits)
-            } else {
-                yield(null to circuits) // Bit ugly, but easiest way I could think of to support both parts of today's problem
-            }
-        }
+        // If not part of same circuit, merge circuits (and update circuit property of junctionboxes)
+        circuits[circuit2]!!.forEach { circuit -> circuit.circuit = circuit1 }
+        circuits[circuit1] = circuits[circuit1]!!.plus(circuits[circuit2]!!)
+        circuits.remove(circuit2)
+
+        lastConnection = connection
     }
+
+    return lastConnection!! to circuits
 }
 
 private fun parseJunctionBoxes(input: List<String>) =
