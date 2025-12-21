f95aec8614c445b6bc9f20900c9e786bf13dd89e
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2025-12-21 15:43:57 +0100

    Minor refactor day 06 2023

--- advent-of-code-2023/src/main/kotlin/day06.kt
+++ advent-of-code-2023/src/main/kotlin/day06.kt
@@ -1,25 +1,25 @@
-fun day06a(input: List<String>): Long {
+fun day06a(input: List<String>): Int {
     val durations = input[0].split(':')[1].split(' ').filter { it.isNotEmpty() }.map(String::toLong)
     val records = input[1].split(':')[1].split(' ').filter { it.isNotEmpty() }.map(String::toLong)
 
     return durations.zip(records)
         .map { (duration, record) -> countNumberOfWaysToWin(duration, record) }
-        .reduce(Long::times)
+        .reduce(Int::times)
 }
 
-fun day06b(input: List<String>): Long {
+fun day06b(input: List<String>): Int {
     val duration = input[0].split(':')[1].replace(" ", "").toLong()
     val record = input[1].split(':')[1].replace(" ", "").toLong()
 
     return countNumberOfWaysToWin(duration, record)
 }
 
-private fun countNumberOfWaysToWin(duration: Long, record: Long): Long {
+private fun countNumberOfWaysToWin(duration: Long, record: Long): Int {
     fun beatsRecord(pressTime: Long) =
         pressTime * (duration - pressTime) > record
 
     val minPressTimeToWin = (1 until duration).first(::beatsRecord)
     val maxPressTimeToWin = (duration - 1 downTo minPressTimeToWin).first(::beatsRecord)
 
-    return maxPressTimeToWin - minPressTimeToWin + 1
+    return (minPressTimeToWin .. maxPressTimeToWin).count()
 }
