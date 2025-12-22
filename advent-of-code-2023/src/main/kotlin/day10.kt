d10d1f2b6552bae3867e38af8249a2cd44b8ad97
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2025-12-22 16:45:21 +0100

    Refactored day 10 2023 a bit

--- advent-of-code-2023/src/main/kotlin/day10.kt
+++ advent-of-code-2023/src/main/kotlin/day10.kt
@@ -26,14 +26,15 @@ fun day10b(input: List<String>): Int {
                     && x / 2 in input[0].indices && y / 2 in input.indices  // Ignore positions that are outside of the input grid
             }
 
-
     return nrOfTotalPositions - nrOfOutsidePositions - nrOfLoopPositions
 }
 
-private fun canMove(x: Int, y: Int, dx: Int, dy: Int, input: List<String>): Boolean {
-    if (y + dy !in input.indices || x + dx !in input[0].indices) return false
+private fun List<String>.canMove(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    if (y2 !in this.indices || x2 !in this[0].indices) return false
 
-    val char = input[y + dy][x + dx]
+    val char = this[y2][x2]
+    val dx = x2 - x1
+    val dy = y2 - y1
 
     return when (char) {
         '|' -> dx == 0 && dy != 0
@@ -61,22 +62,12 @@ private fun findLoop(startX: Int, startY: Int, input: List<String>): Map<Pair<In
     // Traverse pipes from start position and return positions and distances to those positions
     val toVisit = mutableListOf<Pair<Pair<Int, Int>, Int>>()
     toVisit.addAll(
-        listOf(
-            listOf(startX, startY, -1, 0),
-            listOf(startX, startY, 1, 0),
-            listOf(startX, startY, 0, 1),
-            listOf(startX, startY, 0, -1),
-        ).filter { (x, y, dx, dy) ->
-            canMove(x, y, dx, dy, input)
-        }.map { (x, y, dx, dy) ->
-            (x + dx to y + dy) to 1
-        }
+        input[startY][startX]
+            .getNeighbours()
+            .applyDeltas(startX, startY)
+            .filter { (x, y) -> input.canMove(startX, startY, x, y) }
+            .map { pos -> pos to 1 }
     )
-
-    val up = 0 to -1
-    val down = 0 to 1
-    val left = -1 to 0
-    val right = 1 to 0
 
     val distances = mutableMapOf((startX to startY) to 0)
 
@@ -88,23 +79,14 @@ private fun findLoop(startX: Int, startY: Int, input: List<String>): Map<Pair<In
         distances[pos] = distance
 
         val (x, y) = pos
-        val char = input[y][x]
-
-        val moves = when (char) {
-            '|' -> listOf(up, down)
-            '-' -> listOf(left, right)
-            'L' -> listOf(up, right)
-            'J' -> listOf(up, left)
-            '7' -> listOf(down, left)
-            'F' -> listOf(down, right)
-            else -> emptyList()
-        }
 
         toVisit.addAll(
-            moves
-                .filter { (dx, dy) -> canMove(x, y, dx, dy, input) }
-                .map { (dx, dy) -> (x + dx to y + dy) to distance + 1 }
-                .filter { (pos, _) -> pos !in distances }
+            input[y][x]
+                .getNeighbours()
+                .applyDeltas(x, y)
+                .filter { (x2, y2) -> input.canMove(x, y, x2, y2) }
+                .map { (x2, y2) -> (x2 to y2) to distance + 1 }
+                .filter { (pos2, _) -> pos2 !in distances }
         )
     }
 
@@ -113,30 +95,19 @@ private fun findLoop(startX: Int, startY: Int, input: List<String>): Map<Pair<In
 
 private fun Set<Pair<Int, Int>>.zoomLoop(input: List<String>): Set<Pair<Int, Int>> {
     // Zooms in by multiplying all positions by two and filling in connections between the gaps
-    val expandedLoop = mutableSetOf<Pair<Int, Int>>()
-    this.forEach { (x, y) ->
-        val expandedX = x * 2
-        val expandedY = y * 2
+    val zoomedLoopPositions = mutableSetOf<Pair<Int, Int>>()
+    this.forEach { (originalX, originalY) ->
+        val zoomedX = originalX * 2
+        val zoomedY = originalY * 2
 
-        val left = expandedX - 1 to expandedY
-        val right = expandedX + 1 to expandedY
-        val up = expandedX to expandedY - 1
-        val down = expandedX to expandedY + 1
+        zoomedLoopPositions.add(zoomedX to zoomedY)
+        zoomedLoopPositions.addAll(
+            input[originalY][originalX].getNeighbours()
+                .applyDeltas(zoomedX, zoomedY)
 
-        expandedLoop.add(expandedX to expandedY)
-        expandedLoop.addAll(
-            when (input[y][x]) {
-                '|' -> listOf(up, down)
-                '-' -> listOf(left, right)
-                'L' -> listOf(up, right)
-                'J' -> listOf(up, left)
-                '7' -> listOf(down, left)
-                'F' -> listOf(down, right)
-                else -> emptyList()
-            }
         )
     }
-    return expandedLoop
+    return zoomedLoopPositions
 }
 
 private fun findZoomedOutsidePositions(expandedLoop: Set<Pair<Int, Int>>, input: List<String>): Set<Pair<Int, Int>> {
@@ -167,3 +138,24 @@ private fun findZoomedOutsidePositions(expandedLoop: Set<Pair<Int, Int>>, input:
 
     return outsidePositions
 }
+
+private fun Char.getNeighbours(): List<Pair<Int, Int>> {
+    val up = 0 to -1
+    val down = 0 to 1
+    val left = -1 to 0
+    val right = 1 to 0
+
+    return when (this) {
+        '|' -> listOf(up, down)
+        '-' -> listOf(left, right)
+        'L' -> listOf(up, right)
+        'J' -> listOf(up, left)
+        '7' -> listOf(down, left)
+        'F' -> listOf(down, right)
+        'S' -> listOf(up, down, left, right)
+        else -> emptyList()
+    }
+}
+
+private fun List<Pair<Int, Int>>.applyDeltas(x: Int, y: Int) =
+    this.map { (dx, dy) -> x + dx to y + dy }
