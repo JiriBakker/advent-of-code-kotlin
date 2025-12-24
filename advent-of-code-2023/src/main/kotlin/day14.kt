0d9280ca97a2bf3647437d987dc00d3c2a40717e
Jiri Bakker <8235125+JiriBakker@users.noreply.github.com>
2025-12-24 22:18:14 +0100

    Implemented 2023 day 14

---
+++ advent-of-code-2023/src/main/kotlin/day14.kt
@@ -0,0 +1,95 @@
+import util.sumOfLong
+
+fun day14a(input: List<String>): Long {
+    val grid = input.map { it.toMutableList() }
+
+    grid.moveAll(0, -1)
+
+    return grid.measureWeight()
+}
+
+fun day14b(input: List<String>): Long {
+    val grid = input.map { it.toMutableList() }
+
+    val weights = (0 until 100_000)
+        .map {
+            grid.moveAll(0, -1)
+            grid.moveAll(-1, 0)
+            grid.moveAll(0, 1)
+            grid.moveAll(1, 0)
+            grid.measureWeight()
+        }
+
+    return weights
+        .findCycle()
+        .extrapolateTo(999_999_999, weights)
+}
+
+private fun List<MutableList<Char>>.moveAll(dx: Int, dy: Int) {
+    var y = if (dy == -1) 0 else this.size - 1
+    var x = if (dx == -1) 0 else this[0].size - 1
+
+    if (dx != 0) {
+        while (x in this[0].indices) {
+            for (y in this.indices) {
+                if (this[y][x] == 'O') {
+                    this.move(x, y, dx, dy)
+                }
+            }
+            x += -dx
+        }
+    } else {
+        while (y in this.indices) {
+            for (x in this[0].indices) {
+                if (this[y][x] == 'O') {
+                    this.move(x, y, dx, dy)
+                }
+            }
+            y += -dy
+        }
+    }
+}
+
+
+private fun List<MutableList<Char>>.move(x: Int, y: Int, dx: Int, dy: Int) {
+    var curX = x + dx
+    var curY = y + dy
+
+    while (curX in this[0].indices && curY in this.indices) {
+        if (this[curY][curX] != '.') return
+        this[curY][curX] = 'O'
+        this[curY - dy][curX - dx] = '.'
+
+        curX += dx
+        curY += dy
+    }
+}
+
+private fun List<List<Char>>.measureWeight(): Long {
+    return this.indices.sumOfLong { y ->
+        this[y].indices.sumOfLong { x ->
+            if (this[y][x] == 'O') this.size - y.toLong()
+            else 0
+        }
+    }
+}
+
+private fun List<Long>.findCycle(): Pair<Int, Int> {
+    for (cycleSize in 2 until this.size) {
+        val firstOffsetWithCycle = this.indices.firstOrNull { offset -> this.isCycle(offset, cycleSize) }
+        if (firstOffsetWithCycle != null) {
+            return firstOffsetWithCycle to cycleSize
+        }
+    }
+    throw Exception("No cycle found")
+}
+
+private fun List<Long>.isCycle(offset: Int, cycleSize: Int): Boolean {
+    if (offset + cycleSize + cycleSize >= this.size) return false
+    return this.subList(offset, offset + cycleSize) == this.subList(offset + cycleSize, offset + cycleSize + cycleSize)
+}
+
+private fun Pair<Int, Int>.extrapolateTo(cycle: Int, weights: List<Long>): Long {
+    val (offset, cycleSize) = this
+    return weights[(cycle - offset) % cycleSize + offset]
+}
---
+++ advent-of-code-2023/src/test/kotlin/day14.kt
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import util.readInputLines
+
+class Day14aTests {
+
+    @Test
+    fun testExampleInput1() {
+        assertEquals(136, day14a(listOf(
+            "O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#....")))
+    }
+
+    @Test
+    fun testActualInput() {
+        assertEquals(105623, day14a(readInputLines("day14")))
+    }
+}
+
+class Day14bTests {
+    @Test
+    fun testExampleInput1() {
+        assertEquals(64, day14b(listOf(
+            "O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#....")))
+    }
+
+    @Test
+    fun testActualInput() {
+        assertEquals(36771, day14b(readInputLines("day14")))
+    }
+}
